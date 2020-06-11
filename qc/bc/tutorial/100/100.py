fc2d790155b31509ae64cf4c5966c2c5fa6caa82
hiroshi0530 <whiroshi0530@gmail.com>
2020-06-11 15:25:26 +0900

    qc の追加

---
+++ qc/bc/tutorial/100/100.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+get_ipython().system('pip install blueqat')
+
+
+# In[3]:
+
+
+from blueqat import Circuit
+
+
+# In[5]:
+
+
+Circuit().x[0].m[:].run(shots=100)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
