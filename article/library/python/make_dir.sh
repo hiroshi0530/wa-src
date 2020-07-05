25fb9c73e84f3a489a9e8ea1a0296040cf49dc8c
hiroshi0530 <whiroshi0530@gmail.com>
2020-07-05 13:14:16 +0900

    [auto] update

--- article/library/python/make_dir.sh
+++ article/library/python/make_dir.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in `seq -f %03g 3 5`; do
+for i in `seq -f %03g 6 7`; do
   if [ ! -d $i ]; then mkdir $i; fi
   if [ ! -f $i/${i}_nb.ipynb ]; then cp template.ipynb $i/${i}_nb.ipynb; fi
 done
