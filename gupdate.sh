3f501fe248a94d04e7c9c3abd1cfdc2121c386c1
hiroshi0530 <whiroshi0530@gmail.com>
2020-07-08 18:32:10 +0900

    [auto] update

--- gupdate.sh
+++ gupdate.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
+#!/bin/bash -l
 
-git add . && git commit -m "[auto] update" && git push origin master
+cd ~/wa/ && git add . && git commit -m "[auto] update" && git push origin master
+cd ~/wa_src/ && git add . && git commit -m "[auto] update" && git push origin master
+cd ~/wa/static/ && source .envrc && bash deploy.sh
