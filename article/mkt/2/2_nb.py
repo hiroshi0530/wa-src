1d52ff91f72f4463af4c8ec15283e9c1d81aa087
hiroshi0530 <whiroshi0530@gmail.com>
2020-07-09 02:28:07 +0900

    [auto] update

--- article/mkt/2/2_nb.py
+++ article/mkt/2/2_nb.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+
 # coding: utf-8
 
 # ## 準備
@@ -35,8 +35,8 @@ import numpy as np
 import scipy
 from scipy.stats import binom
 
-get_ipython().run_line_magic('matplotlib', 'inline')
-get_ipython().run_line_magic('config', "InlineBackend.figure_format = 'svg'")
+get_ipython().magic('matplotlib inline')
+get_ipython().magic("config InlineBackend.figure_format = 'svg'")
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -429,7 +429,7 @@ print('covariances : ', covariances)
 # を得ます。
 
 # #### python code
-# pythonのコードは以下の通りです。私の[git repository](https://github.com/hiroshi0530/curv-api/blob/master/function/py/api/curv-get-k-from-P0-m.py)も参考にしてください。
+# pythonのコードは以下の通りです。
 
 # In[5]:
 
