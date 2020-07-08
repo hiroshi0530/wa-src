077222b7db29cd288875302a473b44d0f8f47255
hiroshi0530 <whiroshi0530@gmail.com>
2020-07-08 18:39:39 +0900

    [auto] update

--- gdeploy.sh
+++ gdeploy.sh
@@ -2,3 +2,4 @@
 
 cd ~/wa/ && git add . && git commit -m "[auto] update" && git push origin master
 cd ~/wa_src/ && git add . && git commit -m "[auto] update" && git push origin master
+cd ~/wa/static/ && source .envrc && bash deploy.sh
