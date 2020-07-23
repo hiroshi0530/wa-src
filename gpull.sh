2e40e551838f8c38bf1b05c2c99ac74555be2184
hiroshi0530 <whiroshi0530@gmail.com>
2020-07-23 12:25:15 +0900

    [auto] update

--- gpull.sh
+++ gpull.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -l
 
+#タイミングによって 余計なファイルが入ってしまうので、一度コピージョブをkill
+pkill -kill -f copy_md_to_hugo
+pkill -kill -f execute_copy
+
 cd ~/wa/ && git pull origin master
 cd ~/wa_src/ && git pull origin master
--- gupdate.sh
+++ gupdate.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -l
 
+#タイミングによって 余計なファイルが入ってしまうので、一度コピージョブをkill
+pkill -kill -f copy_md_to_hugo
+pkill -kill -f execute_copy
+
 cd ~/wa/ && git add . && git commit -m "[auto] update" && git push origin master
 cd ~/wa_src/ && git add . && git commit -m "[auto] update" && git push origin master
