25140248f8eb26863450e472ce5580e2e9f4fb1c
hiroshi.wayama <whiroshi0530@gmail.com>
2020-08-07 16:19:34 +0900

    [auto] update

--- gupdate.sh
+++ gupdate.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -l
 
-#タイミングによって 余計なファイルが入ってしまうので、一度コピージョブをkill
+# タイミングによって 余計なファイルが入ってしまうので、一度コピージョブをkill
 pkill -kill -f copy_md_to_hugo
 pkill -kill -f execute_copy
 
-cd ~/wa/ && git add . && git commit -m "[auto] update" && git push origin master
-cd ~/wa_src/ && git add . && git commit -m "[auto] update" && git push origin master
+# 別途手動でコミットしてしまうと、pushしないので分ける
+cd ~/wa/ && git add . && git commit -m "[auto] update"
+cd ~/wa/ && git push origin master
+
+cd ~/wa_src/ && git add . && git commit -m "[auto] update"
+cd ~/wa_src/ && git push origin master
