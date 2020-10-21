37a090190c94fb542be7abb169bd377b7121274d
hiroshi.wayama <whiroshi0530@gmail.com>
2020-10-21 19:59:25 +0900

    [auto] update

--- qc/bq/tutorial/100/100_nb.py
+++ qc/bq/tutorial/100/100_nb.py
@@ -1,39 +1,66 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# ## blueqat tutorial 100番代
+# 
+# 今まで自己流でやってきましたが、一度他の方々がどのような考え方やコーディングをしているのか勉強してみようと思ってやってみました。本書は実際の業務に活用する上でとても参考になる内容だと思っています。データ分析に関わる仕事をしたい方にお勧めしたいです。
+# 
+# ### github
+# - jupyter notebook形式のファイルは[こちら](https://github.com/hiroshi0530/wa-src/blob/master/ml/data100/05/05_nb.ipynb)
+# 
+# ### google colaboratory
+# - google colaboratory で実行する場合は[こちら](https://colab.research.google.com/github/hiroshi0530/wa-src/blob/master/ml/data100/05/05_nb.ipynb)
+# 
+# ### 筆者の環境
+
+# In[1]:
+
+
+get_ipython().system('sw_vers')
+
+
 # In[2]:
 
 
-get_ipython().system('pip install blueqat')
-
-
-# In[2]:
-
-
-from blueqat import Circuit
+get_ipython().system('python -V')
 
 
 # In[3]:
 
 
-Circuit().h[0].m[:].run(shots=200)
+from blueqat import __version__
+print('blueqat version : ', __version__)
 
+
+# 基本的なライブラリをインポートしそのバージョンを確認しておきます。
 
 # In[4]:
 
 
-Circuit().h[0].m[:].run(shots=100)
+get_ipython().run_line_magic('matplotlib', 'inline')
+get_ipython().run_line_magic('config', "InlineBackend.figure_format = 'svg'")
+
+import matplotlib
+import matplotlib.pyplot as plt
+import scipy
+import numpy as np
+import pandas as pd
+
+print('matplotlib version :', matplotlib.__version__)
+print('scipy version :', scipy.__version__)
+print('numpy version :', np.__version__)
+print('pandas version :', pd.__version__)
 
 
-# In[ ]:
+# In[5]:
 
 
-Circuit().h[0].m[:].run(shots=200)
-Circuit().h[0].m[:].run(shots=200)
+from blueqat import Circuit
 
 
-# In[ ]:
+# In[6]:
 
 
-
+for i in range(5):
+  print(Circuit().h[0].m[:].run(shots=100))
 
