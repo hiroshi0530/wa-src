c3d57c9cf05b5c2eaa3414eee12453e2b444902c
hiroshi0530 <whiroshi0530@gmail.com>
2020-11-10 23:23:04 +0900

    [auto] update

--- ml/data100/07/07_nb.py
+++ ml/data100/07/07_nb.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+
 # coding: utf-8
 
 # ## 第7章 ロジスティクスネットワークの最適設計を行う10本ノック
@@ -33,8 +33,8 @@ get_ipython().system('python -V')
 # In[3]:
 
 
-get_ipython().run_line_magic('matplotlib', 'inline')
-get_ipython().run_line_magic('config', "InlineBackend.figure_format = 'svg'")
+get_ipython().magic('matplotlib inline')
+get_ipython().magic("config InlineBackend.figure_format = 'svg'")
 
 import matplotlib
 import matplotlib.pyplot as plt
