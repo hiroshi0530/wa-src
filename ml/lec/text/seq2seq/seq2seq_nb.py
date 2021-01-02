b6f09c0cc860d9ccbf8785a4447db1a4c2d560a0
hiroshi.wayama <whiroshi0530@gmail.com>
2021-01-02 19:02:17 +0900

    [auto] update

--- ml/lec/text/seq2seq/seq2seq_nb.py
+++ ml/lec/text/seq2seq/seq2seq_nb.py
@@ -5,7 +5,7 @@
 # 
 # 前回、LSTMによる実装を行いましたので、次はsquence to sequenceモデルを実装していこうと思います。今現在では、機械翻訳などの自然言語処理では、このsequnece to sequenceとAttentionを基本としたモデルがよく利用されています。BERTなどもAttentionモデルが基本となっています。
 # 
-# ここでは、復習もかねて、基本的なsequnece to sequenceを実装します。$y=\exp x$を$y=\log x$に翻訳するモデルの構築を行います。なお、モデルの詳細は検索すればいくらでも出てきますのでここでは割愛します。文献や教科書、技術者によっては、sequnece to sequenceモデルは、「Encoder-Decoderモデル」、「系列変換モデル」などと呼ばれることも多いようです。
+# ここでは、復習もかねて、基本的なsequnece to sequenceを実装します。$y=\sin x$を$y=\cos x$に翻訳するモデルの構築を行います。なお、モデルの詳細は検索すればいくらでも出てきますのでここでは割愛します。文献や教科書、技術者によっては、sequnece to sequenceモデルは、「Encoder-Decoderモデル」、「系列変換モデル」などと呼ばれることも多いようです。
 # 
 # 以下ではkerasを用いてseq2seqの実装を行いますが、詳細は[公式ブログ](https://blog.keras.io/a-ten-minute-introduction-to-sequence-to-sequence-learning-in-keras.html)を参照してください。
 # 
@@ -81,28 +81,22 @@ print('gensim version : ', gensim.__version__)
 # サンプル用のエンコーダーデータ、デコーダーデータとして、以下の式を利用します。
 # 
 # $$
-# \text{encoder} : y = \sqrt{x} 
+# \text{encoder} : y = \sin{x} 
 # $$
 # 
 # $$
-# \text{dencoder} : y = x^2
+# \text{dencoder} : y = \cos x
 # $$
 # 
+# サイン関数をコサイン関数に変換します。
 
 # In[4]:
 
 
-x = np.linspace(0, 1.5, 50)
-seq_out = np.array(x ** 0.5)
-seq_in = np.array(x ** 2)
-
-# x = np.linspace(-2*np.pi, 2*np.pi)  # -2πから2πまで
-# seq_in = np.sin(x)
-# seq_out = np.cos(x)
-
-# x = np.linspace(0, 5 * np.pi, 50)
-# seq_in = np.exp(-x / 5) * (np.cos(x))
-# seq_out = np.exp(-x / 5) * (np.sin(x))
+## seq2seq sin cos
+x = np.linspace(-2*np.pi, 2*np.pi, 100)  # -2πから2πまで
+seq_in = np.sin(x)
+seq_out = np.cos(x)
 
 
 # ### データの確認
@@ -145,14 +139,26 @@ plt.grid()
 plt.show()
 
 
-# ### データの準備
+# ### データとパラメタの準備
 # 
-# kerasに入力するためのデータをnumpy配列に格納します。
+# 学習に利用するハイパーパラメタの値を設定します。
 
 # In[9]:
 
 
-NUM_LSTM = 10 
+# LSTMネットワークのパラメタ
+NUM_LSTM = 24
+NUM_MID = 75
+
+# 学習用のパラメタ
+batch_size = 10
+epochs = 35
+
+
+# kerasに入力するためのデータをnumpy配列に格納します。
+
+# In[10]:
+
 
 n = len(x) - NUM_LSTM
 ex = np.zeros((n, NUM_LSTM))
@@ -160,9 +166,9 @@ dx = np.zeros((n, NUM_LSTM))
 dy = np.zeros((n, NUM_LSTM))
 
 for i in range(0, n):
-  ex[i] = seq_in[i:i+NUM_LSTM]
-  dx[i, 1:] = seq_out[i:i+NUM_LSTM-1]
-  dy[i] = seq_out[i:i+NUM_LSTM]
+  ex[i] = seq_in[i : i + NUM_LSTM]
+  dx[i, 1: ] = seq_out[i: i + NUM_LSTM-1]
+  dy[i] = seq_out[i: i + NUM_LSTM]
 
 ex = ex.reshape(n, NUM_LSTM, 1)
 dx = dx.reshape(n, NUM_LSTM, 1)
@@ -173,8 +179,10 @@ dy = dy.reshape(n, NUM_LSTM, 1)
 # 
 # sequence to sequenceのモデルをkerasを用いて実装します。
 # 単純なRNNやLSTMとは異なり、モデルが複数あり、それぞれから入力する必要があるため、Sequenceではなく、Modelを利用します。
+# 
+# 詳細はkerasの[公式ブログ](https://blog.keras.io/a-ten-minute-introduction-to-sequence-to-sequence-learning-in-keras.html)を参照にしてください。
 
-# In[10]:
+# In[11]:
 
 
 from tensorflow.keras.layers import LSTM
@@ -182,11 +190,11 @@ from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.models import Model
 
-NUM_MID = 20
-
 e_input = Input(shape=(NUM_LSTM, 1))
 e_lstm = LSTM(NUM_MID, return_state=True)
 e_output, e_state_h, e_state_c = e_lstm(e_input)
+
+# decoeder側に隠れ層とセルの状態を受け渡す必要があるので、リストを作成
 e_state = [e_state_h, e_state_c]
 
 d_input = Input(shape=(NUM_LSTM, 1))
@@ -196,7 +204,10 @@ d_dense = Dense(1, activation='linear')
 d_output = d_dense(d_output)
 
 seq2seq_model = Model([e_input, d_input], d_output)
-seq2seq_model.compile(loss="mean_squared_error", optimizer="adam")
+
+# 最適化のモデルと損失関数の定義=>どちらでも大きな変化なし
+seq2seq_model.compile(optimizer="adam", loss="mean_squared_error")
+# seq2seq_model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
 
 # モデルの確認
 print(seq2seq_model.summary())
@@ -204,12 +215,8 @@ print(seq2seq_model.summary())
 
 # ## モデルの学習
 
-# In[11]:
+# In[12]:
 
-
-# 学習用のパラメタを設定します
-batch_size = 8
-epochs = 30
 
 history = seq2seq_model.fit([ex, dx], dy, epochs=epochs, batch_size=batch_size, verbose=False)
 
@@ -218,7 +225,7 @@ history = seq2seq_model.fit([ex, dx], dy, epochs=epochs, batch_size=batch_size, 
 # 
 # 損失関数が減少していく様子を可視化してみます。
 
-# In[12]:
+# In[13]:
 
 
 loss = history.history['loss']
@@ -232,97 +239,179 @@ plt.show()
 
 # ## 予測するためのencoderとdecoderのモデルを返す関数の作成
 # 
-# ### encoderモデルの構築
-
-# In[13]:
-
-
-# encoderのモデルを構築
-e_model = Model(e_input, e_state)
-
+# ### 予測用のencoderモデルの構築
+# 
+# インプットするインスタンスや隠れ層やセルの状態は訓練済みのものを利用します。
 
 # In[14]:
 
 
-# decoderのモデルを構築
-d_input = Input(shape=(1, 1))
-
-d_state_in_h = Input(shape=(NUM_MID,))
-d_state_in_c = Input(shape=(NUM_MID,))
-d_state_in = [d_state_in_h, d_state_in_c]
-
-d_output, d_state_h, d_state_c = d_lstm(d_input, initial_state=d_state_in)
-                                                                 
-d_state = [d_state_h, d_state_c]
-
-d_output = d_dense(d_output)
-d_model = Model([d_input] + d_state_in, [d_output] + d_state)
+pred_e_model = Model(e_input, e_state)
 
 
-# データを変換するための関数を実装します。
+# ### 予測用のdecoderモデルの構築
 
 # In[15]:
 
 
-def translate(input_data):
-  state_value = e_model.predict(input_data)
-  y_decoder = np.zeros((1, 1, 1))
-  translated = []
-  
-  for i in range(0, NUM_LSTM):
-    y, h, c = d_model.predict([y_decoder] + state_value)
-    y = y[0][0][0]
-    translated.append(y)
-    dy[0][0][0] = y
-    state_value = [h, c]
+# decoderのモデルを構築
+pred_d_input = Input(shape=(1, 1))
 
-  return translated
+pred_d_state_in_h = Input(shape=(NUM_MID,))
+pred_d_state_in_c = Input(shape=(NUM_MID,))
+pred_d_state_in = [pred_d_state_in_h, pred_d_state_in_c]
+
+# 学習の時に利用したLSTMを利用
+pred_d_output, pred_d_state_h, pred_d_state_c = d_lstm(pred_d_input, initial_state=pred_d_state_in)
+pred_d_state = [pred_d_state_h, pred_d_state_c]
+
+# 学習の時に利用したDENSE層を利用
+pred_d_output = d_dense(pred_d_output)
+pred_d_model = Model([pred_d_input] + pred_d_state_in, [pred_d_output] + pred_d_state)
 
 
-# 結果の確認
+# ## 予測用関数の定義
+# 
+# インプットデータを受け取り、それをdecoderで変換して、結果を出力する関数を構築します。
 
 # In[16]:
 
 
-demo_idices = [0, 13, 26, 39]  # デモに使うデータのインデックス
-# demo_idices = [0, 13, 26, 39]  # デモに使うデータのインデックス
-# demo_idices = [0, 20, 40, 60, 80]  # デモに使うデータのインデックス
-
-for i in demo_idices:
-  x_demo = ex[i : i + 1]
-  y_demo = translate(x_demo)
+def get_output_data(input_data):
+  state_value = pred_e_model.predict(input_data)
+  _dy = np.zeros((1, 1, 1))
   
-  plt.plot(x[i : i + NUM_LSTM], x_demo.reshape(-1), color="b")
-  plt.plot(x[i : i + NUM_LSTM], y_demo, color="r")
+  output_data = []
+  for i in range(0, NUM_LSTM):
+    y_output, y_state_h, y_state_c = pred_d_model.predict([_dy] + state_value)
+    
+    output_data.append(y_output[0, 0, 0])
+    dy[0, 0, 0] = y_output
+    state_value = [y_state_h, y_state_c]
 
-plt.grid()
-plt.show()  
+  return output_data
 
+
+# ## 結果の確認
+# 
 
 # In[17]:
 
 
-plt.plot(x, seq_in, label='$y=\exp x$')
-plt.plot(x, seq_out, label='$y=\log x$')
-plt.legend()
+init_points = [0, 24, 49, 74]
+
+for i in init_points:
+  _x = ex[i : i + 1]
+  _y = get_output_data(_x)
+    
+  if i == 0:
+    plt.plot(x[i : i + NUM_LSTM], _x.reshape(-1), color="b", label='input')
+    plt.plot(x[i : i + NUM_LSTM], _y, color="r", label='output')
+  else:
+    plt.plot(x[i : i + NUM_LSTM], _x.reshape(-1), color="b")
+    plt.plot(x[i : i + NUM_LSTM], _y, color="r")
+  
+plt.plot(x, seq_out, color = 'r', linestyle = "dashed", label = 'correct')
 plt.grid()
-plt.show()
+plt.legend()
+plt.show()  
 
 
-# In[ ]:
+# 何となく形状は捉えているような感じがしますが、どうでしょうか？もう少し、突き詰めれば良い変換が出来そうですが、今回はデモなので子ここまでにしておきます。
+
+# ## 様々なseq2seq
+# 
+# 最後のコードを関数化し、様々なインプットに対して、アウトプットがどうなるか見てみます。
+
+# In[18]:
 
 
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Model
 
 
+def main(x, seq_in, seq_out):
 
-# In[ ]:
+  n = len(x) - NUM_LSTM
+  ex = np.zeros((n, NUM_LSTM))
+  dx = np.zeros((n, NUM_LSTM))
+  dy = np.zeros((n, NUM_LSTM))
+
+  for i in range(0, n):
+    ex[i] = seq_in[i : i + NUM_LSTM]
+    dx[i, 1: ] = seq_out[i: i + NUM_LSTM-1]
+    dy[i] = seq_out[i: i + NUM_LSTM]
+
+  ex = ex.reshape(n, NUM_LSTM, 1)
+  dx = dx.reshape(n, NUM_LSTM, 1)
+  dy = dy.reshape(n, NUM_LSTM, 1)
+  
+  e_input = Input(shape=(NUM_LSTM, 1))
+  e_lstm = LSTM(NUM_MID, return_state=True)
+  e_output, e_state_h, e_state_c = e_lstm(e_input)
+    
+  # encoder stateのリスト化
+  e_state = [e_state_h, e_state_c]
+  
+  d_input = Input(shape=(NUM_LSTM, 1))
+  d_lstm = LSTM(NUM_MID, return_sequences=True, return_state=True)
+  d_output, _, _ = d_lstm(d_input, initial_state=e_state)
+  d_dense = Dense(1, activation='linear')
+  d_output = d_dense(d_output)
+  
+  seq2seq_model = Model([e_input, d_input], d_output)
+  seq2seq_model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+  
+  ## 予測
+  history = seq2seq_model.fit([ex, dx], dy, epochs=epochs, batch_size=batch_size, verbose=False)
+  
+  ## 結果確認
+  init_points = [0, 24, 49, 74]
+  
+  for i in init_points:
+    _x = ex[i : i + 1]
+    _y = get_output_data(_x)
+      
+    if i == 0:
+      plt.plot(x[i : i + NUM_LSTM], _x.reshape(-1), color="b", label='input')
+      plt.plot(x[i : i + NUM_LSTM], _y, color="r", label='output')
+    else:
+      plt.plot(x[i : i + NUM_LSTM], _x.reshape(-1), color="b")
+      plt.plot(x[i : i + NUM_LSTM], _y, color="r")
+    
+  plt.plot(x, seq_out, color = 'r', linestyle = "dashed", label = 'correct')
+  plt.grid()
+  plt.legend()
+  plt.show()  
 
 
+# ## seq2seq exp to log
+
+# In[19]:
 
 
+x = np.linspace(0, 5 * np.pi, 100)
+seq_in = np.exp(-x / 5) * (np.cos(x))
+seq_out = np.exp(-x / 5) * (np.sin(x))
 
-# In[ ]:
+main(x, seq_in, seq_out)
 
 
+# ## seq2seq 0.5乗 to 2乗
+
+# In[20]:
 
 
+x = np.linspace(0, 1.5, 100)
+seq_out = np.array(x ** 0.5)
+seq_in = np.array(x ** 2)
+
+main(x, seq_in, seq_out)
+
+
+# 三角関数以外は、もう少しモデルを最適化しないと実用上使えない（特に減衰振動曲線の場合は、位相が180度ずれているのが致命的）と思います。
+# 今回はkerasのseq2seqに慣れる練習なので、ここまでにしておきます。
+# 
+# しつこいですが、詳細は[公式ブログ](https://blog.keras.io/a-ten-minute-introduction-to-sequence-to-sequence-learning-in-keras.html)を参考にしてください。
