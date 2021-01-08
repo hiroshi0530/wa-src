aa47f7b677da5b35a768d793a5d5f6c96e0bc395
hiroshi.wayama <whiroshi0530@gmail.com>
2021-01-08 20:52:14 +0900

    [auto] update

--- article/library/tf/tutorials/detail/text/01_nb.py
+++ article/library/tf/tutorials/detail/text/01_nb.py
@@ -5,46 +5,9 @@
 # 
 # tensorflowが2.0になってチュートリアルも新しくなりました。勉強がてら、すべてのチュートリアルを自分の環境で行ってみたいと思います。コードはほぼチュートリアルのコピーです。その中で気づいた部分や、注意すべき部分がこの記事の付加価値です。
 # 
-# ※実際に勉強するときは、コードをコピーするのではなく、理解しながら自分で入力することをお勧めします。
+# 今回は単語の埋め込み（分散表現の学習）です。リンクこちらです。
 # 
-# ### tf tutorials 解説目次
-# 
-# 1. KerasによるMLの基本
-#     1. [分類問題の初歩](/article/library/tf/tutorials/01/) : ([公式チュートリアル](https://www.tensorflow.org/tutorials/keras/classification?hl=ja)) <= 現在のページ
-#     2. [TF Hubによるテキスト分類](/article/library/tf/tutorials/02/) : ([公式チュートリアル](https://www.tensorflow.org/tutorials/keras/text_classification_with_hub?hl=ja))
-#     3. [前処理されたテキストによるテキスト分類](/article/library/tf/tutorials/03/) : ([公式チュートリアル]())
-#     4. [回帰:燃費を予測する](/article/library/tf/tutorials/04/) : ([公式チュートリアル](https://www.tensorflow.org/tutorials/keras/regression?hl=ja))
-#     5. [オーバーフィットとアンダーフィット](/article/library/tf/tutorials/05/) : ([公式チュートリアル](https://www.tensorflow.org/tutorials/keras/overfit_and_underfit?hl=ja))
-#     6. [保存と読み込み](/article/library/tf/tutorials/06/) : ([公式チュートリアル](https://www.tensorflow.org/tutorials/keras/save_and_load?hl=ja))
-# 2. データの読み込みと前処理
-#     1. CSV
-#     2. NumPy
-#     3. pandas.DataFrame
-#     4. 画像
-#     5. テキスト
-#     6. Unicode
-#     7. TF.Text
-#     8. TFRecordとtf.Example
-#     9. tf.ioを使ったその他の形式
-# 3. Estimator
-# 4. Customization
-# 5. Distributed training
-# 6. images
-#     1. [Covolutional Neural Network]()
-# 7. Text
-#     1. Word embeddings
-#     2. Text classification with an RNN
-#     3. Text generation with an RNN
-#     4. Neural machine translation with attention
-#     5. image captioning
-#     6. transfer model for languages understanding
-# 8. Structured data
-# 9. Generative
-# 
-# ### github
-# - jupyter notebook形式のファイルは[こちら](https://github.com/hiroshi0530/wa/blob/master/src/tf/tutorials/01/01_nb.ipynb)
-# 
-# ### 筆者の環境
+# - https://www.tensorflow.org/tutorials/text/word_embeddings?hl=ja
 
 # In[1]:
 
@@ -58,19 +21,25 @@ get_ipython().system('sw_vers')
 get_ipython().system('python -V')
 
 
-# In[3]:
+# In[7]:
 
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+get_ipython().run_line_magic('config', "InlineBackend.figure_format = 'svg'")
 
 import tensorflow as tf
 import numpy as np
 import pandas as pd
 
-get_ipython().run_line_magic('matplotlib', 'inline')
-get_ipython().run_line_magic('config', "InlineBackend.figure_format = 'svg'")
 import matplotlib
 import matplotlib.pyplot as plt
 
 from tensorflow import keras
+from tensorflow.keras import layers
+
+import tensorflow_datasets as tfds
+tfds.disable_progress_bar()
+
 
 print('tf version     : ', tf.__version__)
 print('keras version  : ', keras.__version__)
@@ -80,220 +49,210 @@ print('matlib version : ',matplotlib.__version__)
 
 
 # ## 単語の埋め込み(Word embeddings)
-# 訓練用のデータとテスト用のデータの読み込みです。
 
-# In[4]:
-
-
-fashion_mnist = keras.datasets.fashion_mnist
-
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-
-
-# In[5]:
-
-
-print('train_images.shape : ', train_images.shape)
-print('train_labels.shape : ', train_labels.shape)
-print('test_images.shape  : ', test_images.shape)
-print('test_labels.shape  : ', test_labels.shape)
-
-
-# In[6]:
-
-
-train_images[0]
-train_labels[0]
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[7]:
-
-
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-
-
-# In[ ]:
-
-
-
-
+# Embddingレイヤーの作成。埋め込みの重みはランダムに初期化。
 
 # In[8]:
 
 
-plt.figure()
-plt.imshow(train_images[0])
-plt.colorbar()
-plt.grid(False)
-plt.show()
+embedding_layer = layers.Embedding(1000, 5)
 
 
-# In[9]:
+# In[26]:
 
 
-train_images = train_images / 255.0
+result = embedding_layer(tf.constant([1, 2, 3]))
+result.numpy()
 
-test_images = test_images / 255.0
 
+# テキストやシーケンスの問題では、入力として`(samples, sequence_lenght)`の二次元整数テンソルとなる。シーケンスのバッチを入力すると、Embeddingレイヤーは`shape=(samples, sequence_length, embedding_dimensionality)`の三次元のFloaｔテンソルを返す。
 
 # In[10]:
 
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-  plt.subplot(5,5,i+1)
-  plt.xticks([])
-  plt.yticks([])
-  plt.grid(False)
-  plt.imshow(train_images[i], cmap=plt.cm.binary)
-  plt.xlabel(class_names[train_labels[i]])
-plt.show()
+result = embedding_layer(tf.constant([[0,1,2],[3,4,5]]))
+result.shape
 
 
-# ## モデルの構築
+# ## 埋め込みを最初から学習する
 # 
-# ### 層の設定
+# IMDBの映画レビューから感情分析器を作成する。tfdsからデータをロードする。
 
-# In[11]:
-
-
-model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(128, activation='relu'),
-    keras.layers.Dense(10, activation='softmax')
-])
+# In[31]:
 
 
-# ### モデルのコンパイル
+(train_data, test_data), info = tfds.load(
+    'imdb_reviews/subwords8k', 
+    split = (tfds.Split.TRAIN, tfds.Split.TEST), 
+    with_info=True, as_supervised=True)
 
-# In[12]:
+
+# エンコーダー(tfds.features.text.SubwordTextEncoder)を取得し、どのような単語が含まれているか見る。
+
+# In[32]:
 
 
-model.compile(optimizer='adam', 
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+encoder = info.features['text'].encoder
 
+
+# In[33]:
+
+
+print(encoder.subwords[:20])
+
+
+# 映画のレビューは長さが異なっている。padded_batchを用いてレビューの長さを標準化する。tf2.2からpadded_shapesは必須ではなくなったようです。デフォルトで自動で最も長い文章に合わせてパディングされるようです。
 
 # In[13]:
 
 
-model.fit(train_images, train_labels, epochs=5)
+train_data
 
 
-# ## 正解率の評価
+# パディングして、データをバッチ化します。
 
 # In[14]:
 
 
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+train_batches = train_data.shuffle(1000).padded_batch(10, padded_shapes=([None],[]))
+test_batches = test_data.shuffle(1000).padded_batch(10, padded_shapes=([None],[]))
 
-print('\nTest accuracy:', test_acc)
 
-
-# ## 予測する
+# 2.2移行はこれで良さそうです。
 
 # In[15]:
 
 
-predictions = model.predict(test_images)
-predictions[0]
+train_batches = train_data.shuffle(1000).padded_batch(10)
+test_batches = test_data.shuffle(1000).padded_batch(10)
 
 
 # In[16]:
 
 
-np.argmax(predictions[0])
+train_batch, train_labels = next(iter(train_batches))
+train_batch.numpy()
 
+
+# 大きさを見てみます。
+
+# In[37]:
+
+
+train_batch.numpy().shape
+
+
+# ## 単純なモデルの構築
+# 
+# - 整数エンコードされた語彙を受け取り、埋め込みベクトルを返す。その結果次元は `(batch, sequence, embedding)`となる。
+# - GlobalAveragePooling1Dはそれぞれのサンプルについて、固定長の出力ベクトルを返す
+# - 16層の全結合隠れ層
 
 # In[17]:
 
 
-test_labels[0]
+embedding_dim = 16
+
+model = keras.Sequential([
+  layers.Embedding(encoder.vocab_size, embedding_dim),
+  layers.GlobalAveragePooling1D(),
+  layers.Dense(16, activation='relu'),
+  layers.Dense(1)
+])
+
+model.summary()
 
 
-# In[18]:
+# ## モデルのコンパイル
+
+# In[39]:
 
 
-def plot_image(i, predictions_array, true_label, img):
-  predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
-  plt.grid(False)
-  plt.xticks([])
-  plt.yticks([])
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
 
-  plt.imshow(img, cmap=plt.cm.binary)
-
-  predicted_label = np.argmax(predictions_array)
-  if predicted_label == true_label:
-      color = 'blue'
-  else:
-      color = 'red'
-
-  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
-                                  100*np.max(predictions_array),
-                                  class_names[true_label]),
-                                  color=color)
-
-def plot_value_array(i, predictions_array, true_label):
-  predictions_array, true_label = predictions_array[i], true_label[i]
-  plt.grid(False)
-  plt.xticks([])
-  plt.yticks([])
-  thisplot = plt.bar(range(10), predictions_array, color="#777777")
-  plt.ylim([0, 1]) 
-  predicted_label = np.argmax(predictions_array)
-
-  thisplot[predicted_label].set_color('red')
-  thisplot[true_label].set_color('blue')
+history = model.fit(
+    train_batches,
+    epochs=10,
+    validation_data=test_batches, validation_steps=20, verbose=0)
 
 
-# In[19]:
+# In[24]:
 
 
-i = 0
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
-plot_image(i, predictions, test_labels, test_images)
-plt.subplot(1,2,2)
-plot_value_array(i, predictions,  test_labels)
+history_dict = history.history
+
+acc = history_dict['accuracy']
+val_acc = history_dict['val_accuracy']
+loss=history_dict['loss']
+val_loss=history_dict['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.figure(figsize=(6,4))
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.grid()
+plt.legend()
 plt.show()
 
+plt.figure(figsize=(6,4))
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.ylim((0.5,1))
+plt.grid()
+plt.show()
+
+
+# ## 学習した埋め込みの取得
+# 
+# 訓練によって、学習され行列を取得します。shapeが`(vocab_size, embedding_dim)`になります。
 
 # In[20]:
 
 
-i = 12
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
-plot_image(i, predictions, test_labels, test_images)
-plt.subplot(1,2,2)
-plot_value_array(i, predictions,  test_labels)
-plt.show()
+e = model.layers[0]
+weights = e.get_weights()[0]
+print(weights.shape) # shape: (vocab_size, embedding_dim)
 
 
-# In[ ]:
+# ## ファイルへ出力
+
+# In[21]:
 
 
+import io
+
+encoder = info.features['text'].encoder
+
+out_v = io.open('vecs.tsv', 'w', encoding='utf-8')
+out_m = io.open('meta.tsv', 'w', encoding='utf-8')
+
+for num, word in enumerate(encoder.subwords):
+  vec = weights[num+1] # 0 はパディングのためスキップ
+  out_m.write(word + "\n")
+  out_v.write('\t'.join([str(x) for x in vec]) + "\n")
+out_v.close()
+out_m.close()
 
 
-
-# In[ ]:
-
+# In[22]:
 
 
+try:
+  from google.colab import files
+except ImportError:
+   pass
+   pass
+else:
+  files.download('vecs.tsv')
+  files.download('meta.tsv')
 
-
-# ## 感想
-# tensorflow 2.0 形式になり、dry-by-runで実行できるようになったため、より直感的に利用できるという印象です。
