eb87814ce1a79b42bc571c460f18aadfe9c72560
hiroshi.wayama <whiroshi0530@gmail.com>
2021-01-14 19:05:27 +0900

    ディレクトリ追加

---
+++ article/library/tf/tutorials/detail/rnn01/rnn01_nb.py
@@ -0,0 +1,352 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## tensorflow tutorials RNN を使ったテキスト分類
+# 
+# tensorflowが2.0になってチュートリアルも新しくなりました。勉強がてら、すべてのチュートリアルを自分の環境で行ってみたいと思います。コードはほぼチュートリアルのコピーです。その中で気づいた部分や、注意すべき部分がこの記事の付加価値です。
+# 
+# - https://www.tensorflow.org/tutorials/text/text_classification_rnn?hl=ja
+
+# In[1]:
+
+
+get_ipython().system('sw_vers')
+
+
+# In[2]:
+
+
+get_ipython().system('python -V')
+
+
+# In[3]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+get_ipython().run_line_magic('config', "InlineBackend.figure_format = 'svg'")
+
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import tensorflow_datasets as tfds
+tfds.disable_progress_bar()
+
+print('tf version     : ', tf.__version__)
+print('keras version  : ', keras.__version__)
+print('numpy version  : ',np.__version__)
+print('pandas version : ',pd.__version__)
+print('matlib version : ',matplotlib.__version__)
+
+
+# ## ヘルパー関数の作成
+# 結果を描画するための関数
+
+# In[4]:
+
+
+def plot_graphs(history, metric):
+  plt.plot(history.history[metric])
+  plt.plot(history.history['val_'+metric], '')
+  plt.xlabel("Epochs")
+  plt.ylabel(metric)
+  plt.legend([metric, 'val_'+metric])
+  plt.show()
+
+
+# In[5]:
+
+
+dataset, info = tfds.load('imdb_reviews/subwords8k', with_info=True, as_supervised=True)
+train_examples, test_examples = dataset['train'], dataset['test']
+
+
+# In[6]:
+
+
+encoder = info.features['text'].encoder
+
+
+# In[7]:
+
+
+'Vocabulary size: {}'.format(encoder.vocab_size)
+
+
+# In[8]:
+
+
+sample_string = 'Hello TensorFlow.'
+
+encoded_string = encoder.encode(sample_string)
+print('Encoded string is {}'.format(encoded_string))
+
+original_string = encoder.decode(encoded_string)
+print('The original string: "{}"'.format(original_string))
+
+
+# In[9]:
+
+
+assert original_string == sample_string
+
+
+# In[ ]:
+
+
+
+
+
+# In[10]:
+
+
+BUFFER_SIZE = 10000
+BATCH_SIZE = 64
+
+
+# In[11]:
+
+
+train_dataset = (train_examples
+                 .shuffle(BUFFER_SIZE)
+                 .padded_batch(BATCH_SIZE, padded_shapes=([None],[])))
+
+test_dataset = (test_examples
+                .padded_batch(BATCH_SIZE,  padded_shapes=([None],[])))
+
+
+# In[12]:
+
+
+train_dataset = (train_examples
+                 .shuffle(BUFFER_SIZE)
+                 .padded_batch(BATCH_SIZE))
+
+test_dataset = (test_examples
+                .padded_batch(BATCH_SIZE))
+
+
+# In[13]:
+
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(encoder.vocab_size, 64),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(1)
+])
+
+
+# In[14]:
+
+
+model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              optimizer=tf.keras.optimizers.Adam(1e-4),
+              metrics=['accuracy'])
+
+
+# In[15]:
+
+
+test_loss, test_acc = model.evaluate(test_dataset)
+
+print('Test Loss: {}'.format(test_loss))
+print('Test Accuracy: {}'.format(test_acc))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+def pad_to_size(vec, size):
+  zeros = [0] * (size - len(vec))
+  vec.extend(zeros)
+  return vec
+
+
+# In[ ]:
+
+
+def sample_predict(sample_pred_text, pad):
+  encoded_sample_pred_text = encoder.encode(sample_pred_text)
+
+  if pad:
+    encoded_sample_pred_text = pad_to_size(encoded_sample_pred_text, 64)
+  encoded_sample_pred_text = tf.cast(encoded_sample_pred_text, tf.float32)
+  predictions = model.predict(tf.expand_dims(encoded_sample_pred_text, 0))
+
+  return (predictions)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+# パディングなしのサンプルテキストの推論
+
+sample_pred_text = ('The movie was cool. The animation and the graphics '
+                    'were out of this world. I would recommend this movie.')
+predictions = sample_predict(sample_pred_text, pad=False)
+print(predictions)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+# パディングありのサンプルテキストの推論
+
+sample_pred_text = ('The movie was cool. The animation and the graphics '
+                    'were out of this world. I would recommend this movie.')
+predictions = sample_predict(sample_pred_text, pad=True)
+print(predictions)
+
+
+# In[ ]:
+
+
+plot_graphs(history, 'accuracy')
+
+
+# In[ ]:
+
+
+plot_graphs(history, 'loss')
+
+
+# In[ ]:
+
+
+
+
+
+# ## 2つ以上の LSTM レイヤー
+
+# In[ ]:
+
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(encoder.vocab_size, 64),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64,  return_sequences=True)),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(1)
+])
+
+
+# In[ ]:
+
+
+model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              optimizer=tf.keras.optimizers.Adam(1e-4),
+              metrics=['accuracy'])
+
+
+# In[ ]:
+
+
+history = model.fit(train_dataset, epochs=10,
+                    validation_data=test_dataset,
+                    validation_steps=30)
+
+
+# In[ ]:
+
+
+test_loss, test_acc = model.evaluate(test_dataset)
+
+print('Test Loss: {}'.format(test_loss))
+print('Test Accuracy: {}'.format(test_acc))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+# パディングなしのサンプルテキストの推論
+
+sample_pred_text = ('The movie was not good. The animation and the graphics '
+                    'were terrible. I would not recommend this movie.')
+predictions = sample_predict(sample_pred_text, pad=False)
+print(predictions)
+
+
+# In[ ]:
+
+
+# パディングありのサンプルテキストの推論
+
+sample_pred_text = ('The movie was not good. The animation and the graphics '
+                    'were terrible. I would not recommend this movie.')
+predictions = sample_predict(sample_pred_text, pad=True)
+print(predictions)
+
+
+# In[ ]:
+
+
+plot_graphs(history, 'accuracy')
+
+
+# In[ ]:
+
+
+plot_graphs(history, 'loss')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
