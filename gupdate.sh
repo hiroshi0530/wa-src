a23e4349ae5968e663a0c3a564aef5f4e146ff1f
hiroshi.wayama <hiroshi.wayama@gmail.com>
2021-05-20 13:05:57 +0900

    [auto] update

--- gupdate.sh
+++ gupdate.sh
@@ -5,8 +5,8 @@ pkill -kill -f copy_md_to_hugo
 pkill -kill -f execute_copy
 
 # 別途手動でコミットしてしまうと、pushしないので分ける
-cd ~/wa/ && git add . && git commit -m "[auto] update"
-cd ~/wa/ && git push origin master
+cd $WA/wa/ && git add . && git commit -m "[auto] update"
+cd $WA/wa/ && git push origin master
 
-cd ~/wa_src/ && git add . && git commit -m "[auto] update"
-cd ~/wa_src/ && git push origin master
+cd $WA/wa_src/ && git add . && git commit -m "[auto] update"
+cd $WA/wa_src/ && git push origin master
