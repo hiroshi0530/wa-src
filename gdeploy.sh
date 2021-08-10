ce135365c9f4ea11606e47549de6a443ee1ced65
hiroshi.wayama <hiroshi.wayama@gmail.com>
2021-08-10 19:40:44 +0900

    [auto] update

--- gdeploy.sh
+++ gdeploy.sh
@@ -4,10 +4,11 @@
 pkill -kill -f copy_md_to_hugo
 pkill -kill -f execute_copy
 
-cd ~/wa/ && git add . && git commit -m "[auto] update"
-cd ~/wa/ && git push origin master
+# 別途手動でコミットしてしまうと、pushしないので分ける
+cd $WA/wa/ && git add . && git commit -m "[auto] update"
+cd $WA/wa/ && git push origin master
 
-cd ~/wa_src/ && git add . && git commit -m "[auto] update"
-cd ~/wa_src/ && git push origin master
+cd $WA/wa_src/ && git add . && git commit -m "[auto] update"
+cd $WA/wa_src/ && git push origin master
 
-cd ~/wa/static/ && source .envrc && bash deploy.sh
+cd $WA/wa/static/ && source .envrc && bash deploy.sh
