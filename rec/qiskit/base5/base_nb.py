041c590d53e9caa47a74de1d1a6ccb1a47b217d4
hiroshi0530 <hiroshi.wayama@gmail.com>
2021-09-17 10:36:24 +0900

    [auto] update

--- rec/qiskit/base5/base_nb.py
+++ rec/qiskit/base5/base_nb.py
@@ -145,56 +145,142 @@ dict(qiskit.__qiskit_version__)
 # 
 # を満たすので、量子フーリエ変換はユニタリ変換になります。
 
-# In[ ]:
-
-
-
-
-
 # $$
 # \begin{aligned}
 # & \frac{1}{\sqrt{2^{n}}} \sum_{k=0}^{2^{n}-1} \exp \left(i \frac{2 \pi k j}{2^{n}}\right)|k\rangle \\
-# &=\frac{1}{\sqrt{2^{n}}} \sum_{k_{1}=0}^{1} \sum_{k_{2}=0}^{1} \cdots \sum_{k_{n}=0}^{1} \exp \left(i \frac{2 \pi j\left(k_{n} k_{n-1} \cdots k_{1}\right)_{2}}{2^{n}}\right)|k_{n}k_{n-1} \cdots k_{1}\rangle \\
-# &=\frac{1}{\sqrt{2^{n}}} \sum_{k_{1}={0}}^1 \sum_{k_{2}=0}^{1} \cdots \sum_{k_{n}=0}^{1} \exp \left(i \cdot 2 \pi j\left(0. k_{n} k_{n-1} \cdots k_{1}\right)_{2}\right)|k_{n} k_{n-1} \cdots k_{1}\rangle\\
-# &=\frac{1}{\sqrt{2^{n}}}\left(\sum_{k_{n}=0}^{1} \exp \left(i^{1} \cdot 2 \pi j\left(0. k_{n}\right)_{2}|k_{n}\rangle \right)\right) \otimes\left(\sum_{k_{n}-0}^{1} \exp \left(i \cdot 2 \pi j\left(0.0 k_{n-1}\right)_{2} \mid k_{n-1}\right)\right) \otimes \cdots\\
-# & \cdots \otimes \left(\sum_{k_{1}=0}^{1} \exp (i \cdot 2 \pi j(0. \underbrace{0 \cdots 0 k_{1}}_{n})|k_{1}\rangle\right)
+# &=\frac{1}{\sqrt{2^{n}}} \sum_{k_{1}=0}^{1} \sum_{k_{2}=0}^{1} \cdots \sum_{k_{n}=0}^{1} \exp \left(i \frac{2 \pi j\left(k_{1} k_{2} \cdots k_{n}\right)_{2}}{2^{n}}\right)|k_{1}k_{2} \cdots k_{n}\rangle \\
+# &=\frac{1}{\sqrt{2^{n}}} \sum_{k_{1}={0}}^1 \sum_{k_{2}=0}^{1} \cdots \sum_{k_{n}=0}^{1} \exp \left(i \cdot 2 \pi j\left(0. k_{1} k_{2} \cdots k_{n}\right)_{2}\right)|k_{1} k_{2} \cdots k_{n}\rangle\\
+# &=\frac{1}{\sqrt{2^{n}}}\left(\sum_{k_{1}=0}^{1} \exp \left(i \cdot 2 \pi j\left(0. k_{1}\right)_{2}|k_{1}\rangle \right)\right) \otimes\left(\sum_{k_{2}=0}^{1} \exp \left(i \cdot 2 \pi j\left(0.0 k_{2}\right)_{2} \mid k_{2}\rangle\right)\right) \otimes \cdots\\
+# & \cdots \otimes \left(\sum_{k_{n}=0}^{1} \exp (i \cdot 2 \pi j(0. \underbrace{0 \cdots 0 k_{n}}_{n})|k_{n}\rangle\right)
 # \end{aligned}
 # $$
+# 
+# ここで、$(\cdots)_2$は２進数表示である事を示します。また、ケットを２進数表記する際、左側から$k_1,k_2, \cdots k_n$とします。
+# 次に$\displaystyle \Sigma$を展開します。
+# 
 
 # $$
 # \begin{aligned}
+# &\frac{1}{\sqrt{2^{n}}}\left(|0\rangle + \exp \left(i \cdot 2 \pi j (0.1 \right)_{2}|1\rangle\right) \otimes \left(|0\rangle + \exp \left(i \cdot 2 \pi j \left(0.01 \right)\_{2}|1\rangle\right) \right) \otimes \cdots \\
+# &\cdots \otimes \left(|0\rangle + \exp \left(i \cdot 2 \pi j (0.0 \cdots 1 \right)\_{2}|1\rangle\right)
 # \end{aligned}
 # $$
 
-# In[ ]:
+# ここで$|j\rangle = |j_{1}j_{2}\cdots j_n\rangle$として、二進数表記すると、以下の様になります。
+# 
+# $$
+# \begin{aligned}
+# &\exp\left(2 \pi j(0.1)\_{2}\right)=\exp\left(2 \pi\left(j\_{1} j\_{2} \cdots j\_{n-1}. j\_{n}\right)\_{2}\right)=\exp\left(2 \pi\left(0. j\_{n}\right)\_{2}\right) \\
+# &\exp\left(2 \pi j(0.01)\_{2}\right)=\exp\left(2 \pi\left(j\_{1} j\_{2} \cdots j\_{n-2} . j\_{n-1} j\_{n}\right)\_{2}\right)=\exp\left(2 \pi\left(0.j\_{n-1} j\_{n}\right)\_{2}\right) \\
+# &\vdots \\
+# &\exp\left(2 \pi\_{j}(0.0 \cdots 01)\_{2}\right)=\exp\left(2 \pi\left(0 . j\_{2} \cdots j\_{n-1} j\_{n}\right)\_{2}\right)\\
+# &\exp\left(2 \pi\_{j}(0.0 \cdots 01)\_{2}\right)=\exp\left(2 \pi\left(0 . j\_{1} j\_{2} \cdots j\_{n-1} j\_{n}\right)\_{2}\right)
+# \end{aligned}
+# $$
+# 
+# 2$\pi$にかかる整数部分は考えなくても良いことがポイントでしょうか。
 
+# 計算を進めると、
+# 
+# $$
+# \begin{aligned}
+# &\frac{1}{\sqrt{2^{n}}}\left( |0\rangle + \exp \left(i \cdot 2 \pi \cdot 0.j_{n} \right)|1\rangle \right) \otimes \left(|0\rangle + \exp \left(i \cdot 2 \pi \cdot 0.j_{n-1}j_{n} \right)|1\rangle  \right) \otimes \cdots\\
+# & \cdots \otimes \left(|0\rangle + \exp \left(i \cdot 2 \pi \cdot 0.j_1j_2\cdots j_n \right)|1\rangle  \right) \,\, \cdots \,\, (1)
+# \end{aligned}
+# $$
 
+# となります。この表現はqiskitのサイトにあるように、
+# 
+# $$
+# \frac{1}{\sqrt{N}}\left(|0\rangle+e^{\frac{2 \pi i}{2} x}|1\rangle\right) \otimes\left(|0\rangle+e^{\frac{2 \pi i}{2^{2}} x}|1\rangle\right) \otimes \ldots \otimes\left(|0\rangle+e^{\frac{2 \pi i}{2^{n-1}} x}|1\rangle\right) \otimes\left(|0\rangle+e^{\frac{2 \pi i}{2^{n} x}}|1\rangle\right) \,\, \cdots \,\, (2)
+# $$
+# 
+# と同じです。次は、これをどのように量子回路で表現するかが次のポイントになります。
 
+# ### 一つ目の回路
+# 一つ目の回路はアダマールゲートです。状態$j$に対して以下の様な作用します。
+# $$
+# H\left|j\right\rangle=\frac{1}{\sqrt{2}}\left(|0\rangle+\exp \left(\frac{2 \pi i}{2} j\right)|1\rangle\right)
+# $$
+# 
+# ### 二つ目の回路
+# 
+# 二つ目がControl-Rotation回路で、コントロールビットに応じて固有値が変化します。CNOTゲートの位相を任意$(2^{-x})$に変更させるゲートになります。
+# 
+# $$
+# \begin{gathered}
+# C R O T_{k}\left|0 x_{j}\right\rangle=\left|0 x_{j}\right\rangle \\
+# C R O T_{k}\left|1 x_{j}\right\rangle=\exp \left(\frac{2 \pi i}{2^{k}} x_{j}\right)\left|1 x_{j}\right\rangle
+# \end{gathered}
+# $$
+# 
+# 行列で表現すると以下の様になります。
+# 
+# $$
+# U R O T_{k}=\left[\begin{array}{lc}
+# 1 & 0 \\
+# 0 & \exp \left(\frac{2 \pi i}{2^{k}}\right)
+# \end{array}\right]
+# $$
 
+# ### 量子回路
+# 
+# 天下りですがアダマールゲートとCROTゲートを2量子ビットに適用してみます。
+# 
+# $$
+# \begin{aligned}
+# CROT_2 \cdot H_1\left|j_1j_2\right\rangle&=CROT_2 \left( \frac{1}{\sqrt{2}}\left(|0\rangle+\exp \left(\frac{2 \pi i}{2} j_1 \right)|1\rangle\right)\otimes|j_2\rangle\right) \\
+# &=\frac{1}{\sqrt{2}}\left(|0\rangle+\exp \left(\frac{2 \pi i}{2}j_1 + \frac{2 \pi i}{2^2} j_2 \right)|1\rangle\otimes|j_2\rangle\right) \\
+# &=\frac{1}{\sqrt{2}}\left(|0\rangle+\exp \left(i \cdot 2 \pi \cdot 0.j_1 + i \cdot 2 \pi \cdot 0.0j_2  \right)|1\rangle\otimes|j_2\rangle\right) \\
+# &=\frac{1}{\sqrt{2}}\left(|0\rangle+\exp \left(i \cdot 2 \pi \cdot 0.j_1j_2  \right)|1\rangle\otimes|j_2\rangle\right) \\
+# \end{aligned}
+# $$
+# 
+# これを$n$ビットに拡張し、CROTゲートも一般化すると、
+# 
+# $$
+# \begin{aligned}
+# CROT_n \cdots CROT_2 \cdot H_1\left|j_1j_2\cdots j_n\right\rangle=\frac{1}{\sqrt{2}}\left(|0\rangle+\exp \left(i \cdot 2 \pi \cdot 0.j_1j_2\cdots j_n  \right)|1\rangle\otimes|j_2j_3\cdots j_n\rangle\right) \\
+# \end{aligned}
+# $$
+# 
+# この式の第一項は式(1)の最後の項と似ています。
+# 上記ではコントロールビットを第一量子ビットにしましたが、これを第二量子ビット、第三量子ビットに変えて同じような作用を行います。
 
-# In[ ]:
+# 次にアダマールゲートを第二量子ビットに作用させ、コントロールビットを第二量子ビットにしてCROTゲートを適用すると、
+# 
+# $$
+# \begin{aligned}
+# & CROT_n \cdots CROT_2 \cdot H_2 \cdot \frac{1}{\sqrt{2}}\left(|0\rangle+\exp \left(i \cdot 2 \pi \cdot 0.j_1j_2\cdots j_n  \right)|1\rangle\otimes|j_2j_3\cdots j_n\rangle\right) \\
+# &=\frac{1}{\sqrt{2}}\left(|0\rangle+\exp \left(i \cdot 2 \pi \cdot 0.j_1j_2\cdots j_n  \right)|1\rangle\otimes|0\rangle+\exp \left(i \cdot 2 \pi \cdot 0.j_2\cdots j_n  \right)|1\rangle\otimes|j_3j_4\cdots j_n\rangle\right) \,\, \cdots \,\, (3)\\
+# \end{aligned}
+# $$
 
-
-
-
+# ここで、$x=j_1j_2\cdots j_n$とすると、
+# 
+# $$
+# i \cdot 2 \pi \cdot 0.j_1j_2j_3\cdots j_n = \frac{2\pi i}{2^n}x  
+# $$
+# $$
+# i \cdot 2 \pi \cdot 0.j_2j_3\cdots j_n = \frac{2\pi i}{2^{n-1}}x  
+# $$
+# $$
+# i \cdot 2 \pi \cdot 0.j_3\cdots j_n = \frac{2\pi i}{2^{n-2}}x  
+# $$
+# 
+# と書けるので、(3)の計算をさらに進めると、
 
 # $$
-# \frac{1}{\sqrt{2^n}}\left(|0\rangle+e^{\frac{2 \pi i}{2} x}|1\rangle\right) \otimes\left(|0\rangle+e^{\frac{2 \pi i}{2^{2}} x}|1\rangle\right) \otimes \ldots \otimes\left(|0\rangle+e^{\frac{2 \pi i}{2^{n-1}} x}|1\rangle\right) \otimes\left(|0\rangle+e^{\frac{2 \pi i}{2^{n}} x}|1\rangle\right)
+# \frac{1}{\sqrt{2^n}}\left(|0\rangle+e^{\frac{2 \pi i}{2^n} x}|1\rangle\right) \otimes\left(|0\rangle+e^{\frac{2 \pi i}{2^{n-1}} x}|1\rangle\right) \otimes \ldots \otimes\left(|0\rangle+e^{\frac{2 \pi i}{2^{2}} x}|1\rangle\right) \otimes\left(|0\rangle+e^{\frac{2 \pi i}{2} x}|1\rangle\right)
 # $$
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+# 
+# とかけます。こちら目的の式(2)と比較して、量子ビットの並びが反転していますが、これはスワップゲートを適用すれば良いだけなので、大きな問題ではありません。
+# 
+# すなわち、量子フーリエ変換はアダマールゲートとCROTゲートを組み合わせることで実現できるという事になります。
 
 # ## qiskitを用いた量子フーリエ変換回路の作成
+# 
+# qiskitを用いて量子フーリエ変換をシミュレートする回路を作って見ようと思います。最も右側のビットに対してアダマールゲートを適用するところから始まります。最後に、スワップゲートを適用して、ビットの状態を交換する必要があります。
 
 # In[57]:
 
@@ -389,107 +475,6 @@ array_to_latex(final_state, pretext="\\\\text{Statevector} = ")
 
 # 以上より、手計算の結果が確認出来ました。
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[15]:
-
-
-sim = Aer.get_backend("aer_simulator")
-qc_init = qc.copy()
-qc_init.save_statevector()
-statevector = sim.run(qc_init).result().get_statevector()
-plot_bloch_multivector(statevector)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# 
-# 
-
-# 
-
-# 
-
-# $$
-# \begin{aligned}
-# &\frac{1}{\sqrt{2^{n}}} \sum_{k_{1}=0}^{1} \cdots \sum_{k_{n}=0}^{1} \exp \left(i \frac{2 \pi\left(k_{1} 2^{n-1}+\cdots k_{n} 2^{0}\right) \cdot j}{2^{n}}\right)\left|k_{1}\right\rangle\left|k_{2}\right\rangle \cdots\left|k_{n}\right\rangle \\
-# &=\frac{1}{\sqrt{2^{n}}} \sum_{k_{1}=0}^{1} \cdots \sum_{k_{n}=0}^{1} \exp \left(i 2 \pi j\left(k_{1} 2^{-1}+\cdots k_{n} 2^{-n}\right)\right)\left|k_{1} k_{2} \cdots k_{n}\right\rangle \\
-# &=\frac{1}{\sqrt{2^{n}}}\left(\sum_{k_{1}=0}^{1} e^{i 2 \pi j k_{1} 2^{-1}}\left|k_{1}\right\rangle\right) \otimes \cdots \otimes\left(\sum_{k_{n}=0}^{1} e^{i 2 \pi j k_{n} 2^{-n}}\left|k_{n}\right\rangle\right) \\
-# &=\frac{1}{\sqrt{2^{n}}}\left(|0\rangle+e^{i 2 \pi 0 . j_{n}}|1\rangle\right) \otimes\left(|0\rangle+e^{i 2 \pi 0 . j_{n-1} j_{n}}|1\rangle\right) \otimes \cdots \\
-# &\otimes\left(|0\rangle+e^{i 2 \pi 0 . j_{2} j_{3} \cdots j_{n}}|1\rangle\right) \otimes\left(|0\rangle+e^{i 2 \pi 0 . j_{1} j_{2} \cdots j_{n}}|1\rangle\right)
-# \end{aligned}
-# $$
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+# ## 計算量について
+# 随時参考にしている、嶋田先生の著書、[量子コンピューティング: 基本アルゴリズムから量子機械学習まで](
+# https://www.amazon.co.jp/%E9%87%8F%E5%AD%90%E3%82%B3%E3%83%B3%E3%83%94%E3%83%A5%E3%83%BC%E3%83%86%E3%82%A3%E3%83%B3%E3%82%B0-%E5%9F%BA%E6%9C%AC%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0%E3%81%8B%E3%82%89%E9%87%8F%E5%AD%90%E6%A9%9F%E6%A2%B0%E5%AD%A6%E7%BF%92%E3%81%BE%E3%81%A7-%E6%83%85%E5%A0%B1%E5%87%A6%E7%90%86%E5%AD%A6%E4%BC%9A%E5%87%BA%E7%89%88%E5%A7%94%E5%93%A1%E4%BC%9A/dp/4274226212#:~:text=OK-,%E9%87%8F%E5%AD%90%E3%82%B3%E3%83%B3%E3%83%94%E3%83%A5%E3%83%BC%E3%83%86%E3%82%A3%E3%83%B3%E3%82%B0%3A%20%E5%9F%BA%E6%9C%AC%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0%E3%81%8B%E3%82%89%E9%87%8F%E5%AD%90%E6%A9%9F%E6%A2%B0%E5%AD%A6%E7%BF%92,%E5%8D%98%E8%A1%8C%E6%9C%AC%20%E2%80%93%202020%2F11%2F9&text=%E3%81%93%E3%82%8C%E3%81%8B%E3%82%89%E5%BF%85%E3%81%9A%E3%81%8F%E3%82%8B%E9%87%8F%E5%AD%90%E3%82%B3%E3%83%B3%E3%83%94%E3%83%A5%E3%83%BC%E3%83%86,%E3%82%84%E3%81%99%E3%81%8F%E8%A7%A3%E8%AA%AC%E3%81%97%E3%81%9F%E6%9B%B8%E7%B1%8D%E3%81%A7%E3%81%99%E3%80%82)によりますと、FFTの計算量が$O(N\log N)$で量子フーリエ変換の計算量が$O((\log N)^2)$なので、一見量子フーリエ変換の高速に見えますが、実行や測定の手間を考えると必ずしも量子フーリエ変換にアドバンテージがあるとは限らないようです。とは言え、最初に考えた人天才ですね。ただただ感嘆するばかりです。
