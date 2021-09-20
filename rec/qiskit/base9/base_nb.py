33d94aabac1d2103d535329e6856ab369ce64e86
hiroshi.wayama <hiroshi.wayama@gmail.com>
2021-09-20 12:00:20 +0900

    [auto] update

--- rec/qiskit/base9/base_nb.py
+++ rec/qiskit/base9/base_nb.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ## 量子位相推定
+# ## HHLアルゴリズム
 # 
 # qiskitを利用して、量子アルゴリズムについて自分なりに勉強していこうと思います。
 # 個人的な勉強の記録なので、説明などを大幅に省いている可能性があります。
 # 
 # qiskitのウェブサイト通りに勉強を進めています。
 # 
-# - https://qiskit.org/textbook/ja/ch-algorithms/quantum-phase-estimation.html
+# - https://qiskit.org/textbook/ja/ch-applications/hhl_tutorial.html
 # 
-# 量子アルゴリズムとしては最も重要な量子位相推定を勉強してみます。様々なアルゴリズムに利用されおり、理解は必須となっています。位相キックバックと量子フーリエ逆変換の組み合わせ、ある固有ベクトルのユニタリ演算子に対する固有値を（その位相）を推定します。
 # 
 # ### github
-# - jupyter notebook形式のファイルは[こちら](https://github.com/hiroshi0530/wa-src/blob/master/rec/qiskit/base6/base_nb.ipynb)
+# - jupyter notebook形式のファイルは[こちら](https://github.com/hiroshi0530/wa-src/blob/master/rec/qiskit/base9/base_nb.ipynb)
 # 
 # ### google colaboratory
-# - google colaboratory で実行する場合は[こちら](https://colab.research.google.com/github/hiroshi0530/wa-src/blob/master/rec/qiskit/base6/base_nb.ipynb)
+# - google colaboratory で実行する場合は[こちら](https://colab.research.google.com/github/hiroshi0530/wa-src/blob/master/rec/qiskit/base9/base_nb.ipynb)
 # 
 # ### 筆者の環境
 
@@ -34,7 +33,7 @@ get_ipython().system('python -V')
 
 # 基本的なライブラリをインポートしそのバージョンを確認しておきます。
 
-# In[9]:
+# In[1]:
 
 
 get_ipython().run_line_magic('matplotlib', 'inline')
@@ -55,67 +54,8 @@ from qiskit.visualization import plot_histogram
 dict(qiskit.__qiskit_version__)
 
 
-# ## 理論部分の理解
-# 
-# 固有ベクトル$|\psi\rangle$のユニタリ演算子に対する固有値$e^{2 \pi i \theta}$を求めます。固有ベクトルやユニタリ演算子は既知とします。
-# 
-# $$
-# U|\psi\rangle=e^{2 \pi i \theta}|\psi\rangle
-# $$
-# 
-# 
-
 # In[ ]:
 
 
 
 
-
-# In[ ]:
-
-
-
-
-
-# In[10]:
-
-
-qpe = QuantumCircuit(4, 3)
-qpe.x(3)
-qpe.draw('mpl')
-
-
-# In[11]:
-
-
-for qubit in range(3):
-    qpe.h(qubit)
-qpe.draw('mpl')
-
-
-# In[12]:
-
-
-repetitions = 1
-for counting_qubit in range(3):
-    for i in range(repetitions):
-        qpe.cp(math.pi/4, counting_qubit, 3);
-    repetitions *= 2
-qpe.draw('mpl')
-
-
-# In[ ]:
-
-
-
-
-
-# In[16]:
-
-
-if 1 <= 2:
-  print('aa')
-  pass
-
-
-# ## 感想
