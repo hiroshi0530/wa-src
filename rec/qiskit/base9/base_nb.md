0e506fc6c850bdbeade2360b46bc89798ca7ec2b
hiroshi0530 <hiroshi.wayama@gmail.com>
2021-09-21 00:06:02 +0900

    [auto] update

--- rec/qiskit/base9/base_nb.md
+++ rec/qiskit/base9/base_nb.md
@@ -7,6 +7,10 @@ qiskitのウェブサイト通りに勉強を進めています。
 
 - https://qiskit.org/textbook/ja/ch-applications/hhl_tutorial.html
 
+私の拙いブログでqiskitがRec（推薦システム）のカテゴライズしいたのは、すべてHHLを理解するためでした。現在、推薦システムに興味があり、開発などを行っていますが、そこで重要なのが連立一次方程式を解く事です。連立一次方程式は、数理モデルをコンピュータを利用してとく場合に高い確率で利用されますが、推薦システムもUser-Item行列から如何にしてユーザーエンゲージメントの高い特徴量を抽出出来るかという事が重要になってきます。
+
+よって、量子コンピュータを利用して高速に連立一次方程式を解く事を目標に量子アルゴリズムの復習を開始したわけですが、ようやく目的までたどり着きました。
+
 
 ### github
 - jupyter notebook形式のファイルは[こちら](https://github.com/hiroshi0530/wa-src/blob/master/rec/qiskit/base9/base_nb.ipynb)
@@ -71,6 +75,37 @@ dict(qiskit.__qiskit_version__)
      'qiskit-machine-learning': None}
 
 
+
+
+```python
+
+
+```
+
+## 理論部分の理解
+
+HHLの仮定
+- ローディングを実施する効果的なオラクルが存在
+- ハミルトニアンシミュレーションと解の関数の計算が可能
+- $A$がエルミート行列
+- $\mathcal{O}\left(\log (N) s^{2} \kappa^{2} / \epsilon\right)$で計算可能
+- 古典アルゴリズムが完全解を返すが、HHLは解となるベクトルを与える関数を近似するだけ
+
+
+```python
+
+```
+
+$\tilde{\theta}$ は $2^{n} \theta$ に対するバイナリー近似を示しており、 $n$ の添え文字は $n$ デ ィジットに切り捨てられていることを示しています。
+
+$$
+\operatorname{QPE}\left(U,|0\rangle_{n}|\psi\rangle_{m}\right)=|\tilde{\theta}\rangle_{n}|\psi\rangle_{m}
+$$
+
+
+```python
+
+```
 
 
 ```python
--- rec/qiskit/base9/base_nb.py
+++ rec/qiskit/base9/base_nb.py
@@ -10,6 +10,10 @@
 # 
 # - https://qiskit.org/textbook/ja/ch-applications/hhl_tutorial.html
 # 
+# 私の拙いブログでqiskitがRec（推薦システム）のカテゴライズしいたのは、すべてHHLを理解するためでした。現在、推薦システムに興味があり、開発などを行っていますが、そこで重要なのが連立一次方程式を解く事です。連立一次方程式は、数理モデルをコンピュータを利用してとく場合に高い確率で利用されますが、推薦システムもUser-Item行列から如何にしてユーザーエンゲージメントの高い特徴量を抽出出来るかという事が重要になってきます。
+# 
+# よって、量子コンピュータを利用して高速に連立一次方程式を解く事を目標に量子アルゴリズムの復習を開始したわけですが、ようやく目的までたどり着きました。
+# 
 # 
 # ### github
 # - jupyter notebook形式のファイルは[こちら](https://github.com/hiroshi0530/wa-src/blob/master/rec/qiskit/base9/base_nb.ipynb)
@@ -19,13 +23,13 @@
 # 
 # ### 筆者の環境
 
-# In[53]:
+# In[2]:
 
 
 get_ipython().system('sw_vers')
 
 
-# In[54]:
+# In[3]:
 
 
 get_ipython().system('python -V')
@@ -52,6 +56,39 @@ from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
 from qiskit.visualization import plot_histogram
 
 dict(qiskit.__qiskit_version__)
+
+
+# In[ ]:
+
+
+
+
+
+# ## 理論部分の理解
+# 
+# HHLの仮定
+# - ローディングを実施する効果的なオラクルが存在
+# - ハミルトニアンシミュレーションと解の関数の計算が可能
+# - $A$がエルミート行列
+# - $\mathcal{O}\left(\log (N) s^{2} \kappa^{2} / \epsilon\right)$で計算可能
+# - 古典アルゴリズムが完全解を返すが、HHLは解となるベクトルを与える関数を近似するだけ
+
+# In[ ]:
+
+
+
+
+
+# $\tilde{\theta}$ は $2^{n} \theta$ に対するバイナリー近似を示しており、 $n$ の添え文字は $n$ デ ィジットに切り捨てられていることを示しています。
+
+# $$
+# \operatorname{QPE}\left(U,|0\rangle_{n}|\psi\rangle_{m}\right)=|\tilde{\theta}\rangle_{n}|\psi\rangle_{m}
+# $$
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
