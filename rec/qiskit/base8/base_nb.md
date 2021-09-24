353ca0c35497a02224185d13ede6081098f350ce
hiroshi.wayama <hiroshi.wayama@gmail.com>
2021-09-25 01:13:39 +0900

    [auto] update

--- rec/qiskit/base8/base_nb.md
+++ rec/qiskit/base8/base_nb.md
@@ -1,4 +1,4 @@
-## 量子位相推定
+## グローバーのアルゴリズム
 
 qiskitを利用して、量子アルゴリズムについて自分なりに勉強していこうと思います。
 個人的な勉強の記録なので、説明などを大幅に省いている可能性があります。
@@ -7,13 +7,17 @@ qiskitのウェブサイト通りに勉強を進めています。
 
 - https://qiskit.org/textbook/ja/ch-algorithms/quantum-phase-estimation.html
 
-量子アルゴリズムとしては最も重要な量子位相推定を勉強してみます。様々なアルゴリズムに利用されおり、理解は必須となっています。位相キックバックと量子フーリエ逆変換の組み合わせ、ある固有ベクトルのユニタリ演算子に対する固有値を（その位相）を推定します。
+グローバーのアルゴリズムは探索問題を非常に高速に行える事で有名です。
+通常のリスト構造を持つ配列の探索問題の場合、逐次探索を実行することで、最良の場合で$O(1)$、最悪の場合で$O(N)$の計算時間を必要とします。
+また、リストがソート済みの場合、二部探索を利用する事で$O(N)$となり、指数関数的に高速になります。
+
+グローバーのアルゴリズムはデータが非構造化であっても、ソートされていなくも$O(\sqrt{N})$の計算速度を実現することが出来ます。
 
 ### github
-- jupyter notebook形式のファイルは[こちら](https://github.com/hiroshi0530/wa-src/blob/master/rec/qiskit/base6/base_nb.ipynb)
+- jupyter notebook形式のファイルは[こちら](https://github.com/hiroshi0530/wa-src/blob/master/rec/qiskit/base8/base_nb.ipynb)
 
 ### google colaboratory
-- google colaboratory で実行する場合は[こちら](https://colab.research.google.com/github/hiroshi0530/wa-src/blob/master/rec/qiskit/base6/base_nb.ipynb)
+- google colaboratory で実行する場合は[こちら](https://colab.research.google.com/github/hiroshi0530/wa-src/blob/master/rec/qiskit/base8/base_nb.ipynb)
 
 ### 筆者の環境
 
@@ -47,10 +51,13 @@ import json
 
 import matplotlib.pyplot as plt
 import numpy as np
-import math
 
-from qiskit import IBMQ, Aer, transpile, assemble
+from qiskit import IBMQ, Aer, assemble, transpile
 from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
+from qiskit.providers.ibmq import least_busy
+
+from qiskit import execute
+from qiskit_textbook.tools import array_to_latex
 
 from qiskit.visualization import plot_histogram
 
@@ -75,88 +82,272 @@ dict(qiskit.__qiskit_version__)
 
 ## 理論部分の理解
 
-固有ベクトル$|\psi\rangle$のユニタリ演算子に対する固有値$e^{2 \pi i \theta}$を求めます。固有ベクトルやユニタリ演算子は既知とします。
+### 振幅増幅
+
+単純に考えると、$n$ビットを用意して、$2^n$個の状態と探索したい値をマッピングさせ、測定したい状態の一つを探索する事が思いつきます。
+ただ、初期状態だと、すべての状態の振幅が同じであるため、重ね合わせの状態から一つの状態を探索するには、平均$2^n$回の測定が必要になります。
+
+それを解決するのが振幅増幅という考え方です。探索したい状態の振幅をユニタリ演算子を適用することで増幅させ、測定したい状態が非常に高い確率で測定される状態を作り出す事です。
+
+ここですべての状態を、ターゲットの状態$(|\omega\rangle)$とそれに直交する状態$(|s'\rangle)$の二つに分解し、その二つが作るベクトル空間を考えます。
+すべての重ね合わせの状態が均等に分布している$|s\rangle$は、ある位相$\theta$を利用して、
 
 $$
-U|\psi\rangle=e^{2 \pi i \theta}|\psi\rangle
+|s\rangle = \cos \theta\left|s^{\prime}\right\rangle + \sin \theta|w\rangle
+$$
+
+と表現できます。以下、この状態から回転の操作を施し、ターゲットベクトルの振幅増幅を行います。
+
+### 回転操作のイメージ図
+
+振幅増幅のアルゴリズムは、以下の$U_f$と$U_s$という操作を行うことで、初期状態では$|s'\angle$の振幅が大きかった状態から$|\omega\rangle$の振幅を最大にする事です。
+
+![svg](base_nb_files_local/grover.svg)
+
+### U_f(反転Us)
+
+$|\omega\rangle$の成分の振幅を反転させます。
+
+$$
+U_{f}=\left(\begin{array}{ll}
+1 & 0 \\
+0 & -1
+\end{array}\right)
+$$
+
+### U_s(sに対する反転)
+
+$|s\rangle$に対して反転させます。対称に反転させるには三つの行程を経る必要があります。
+
+#### 1. -theta だけ回転
+$$
+U_{s_1}=\left(\begin{array}{cc}
+\cos (-\theta) & -\sin (-\theta) \\
+\sin (-\theta) & \cos (-\theta)
+\end{array}\right)
+$$
+
+#### 2. s'軸の振幅を反転
+$$
+U_{s_{2}}=\left(\begin{array}{ll}
+1 & 0 \\
+0 & -1
+\end{array}\right)
+$$
+
+#### 3. thetaだけ回転
+$$
+U_{s_3}=\left(\begin{array}{cc}
+\cos \theta & -\sin \theta \\
+\sin \theta & \cos \theta
+\end{array}\right)
+$$
+
+### 組み合わせ
+
+上記の1-3を組み合わせ、$|s\rangle$に対する対称の反転を表すことが出来きます。
+
+$$
+\begin{aligned}
+U_s=U_{s_3} U_{s_2} U_{s_1} &=\left(\begin{array}{cc}
+\cos \theta & -\sin \theta \\
+\sin \theta & \cos \theta
+\end{array}\right)\left(\begin{array}{cc}
+1 & 0 \\
+0 & -1
+\end{array}\right)\left(\begin{array}{cc}
+\cos \theta & \sin \theta \\
+-\sin \theta & \cos \theta
+\end{array}\right) \\
+&=\left(\begin{array}{cc}
+\cos 2 \theta & 2 \sin \theta \cos \theta \\
+2 \sin \theta \cos \theta & -\cos 2 \theta
+\end{array}\right) \\
+&=\left(\begin{array}{cc}
+\cos 2 \theta & \sin 2 \theta \\
+\sin 2 \theta & -\cos 2 \theta
+\end{array}\right)
+\end{aligned}
+$$
+
+### U_sとU_fの組み合わせ
+
+$$
+\begin{aligned}
+U_{s} U_{f} &=\left(\begin{array}{cc}
+\cos 2 \theta & \sin 2 \theta \\
+\sin 2 \theta & -\cos 2 \theta
+\end{array}\right)\left(\begin{array}{cc}
+1 & 0 \\
+0 & -1
+\end{array}\right) \\
+&=\left(\begin{array}{cc}
+\cos 2 \theta & -\sin 2 \theta \\
+\sin 2 \theta & \cos 2 \theta
+\end{array}\right)
+\end{aligned}
+$$
+
+この行列は、
+
+$$
+|s\rangle = \cos \theta\left|s^{\prime}\right\rangle + \sin \theta|w\rangle
+$$
+
+をさらに$2\theta$だけ回転させる行列なので、
+
+$$
+\begin{aligned}
+&U_s U_{f}\left(\cos \theta\left|s^{\prime}\right\rangle+\sin \theta|\omega\rangle\right) \\
+&=\cos 3 \theta\left|s^{\prime}\right\rangle+\sin 3 \theta|w\rangle
+\end{aligned}
+$$
+
+となり、結果的に$3\theta$の位相を持つ事になります。
+
+一般化して$k$回操作を行うと、
+
+$$
+\begin{aligned}
+&\left(U_{s} V_{f}\right)^{k}\left(\cos \theta\left|s^{\prime}\right\rangle+\sin \theta|\omega\rangle\right)\\
+&=\cos (2 k+1) \theta\left|s^{\prime}\right\rangle+\sin (2 k+1) \theta|\omega\rangle
+\end{aligned}
+$$
+
+通常の探索問題では、$\theta$は非常に小さい（探索対象となる項目は探索するすべての項目と比較してとても小さい）ですが、何度もこの操作を繰り返すと$2k+1$倍されて、$|\omega\rangle$の振幅が大きくなっていきます。
+
+### 測定回数の目安
+
+ここで、$\displaystyle (2k + 1)\theta \approx \frac{\pi}{2}$の時に、$|\omega\rangle$が検出される確率が最大になるので、
+
+$$
+k = \frac{\pi}{4\theta} - \frac{1}{2}
+$$
+
+が目安となる回転の操作の回数となる。ここで、$\theta$が非常に小さいという仮定があるので、探索対象となる項目数が$M$で、全項目数が$N$とすると、
+
+$$
+\theta \sim \sin\theta = \sqrt{\frac{M}{N}}
+$$
+
+となるので、
+
+$$
+k \sim O\left(\sqrt{\frac{N}{M}}\right)
+$$
+
+という測定回数で、対称のデータに対して確率振幅を最大化する事ができる事が分かります。
+
+## qiskitで実装
+
+2量子ビットの場合に$U_f$や$U_s$がどのような回路になるか、実際に試してみたいと思います。
+探索ターゲットは$|11\angle$とします。
+
+### U_f
+
+$U_f$については、ターゲットビットだけを反転させれば良いので、CZゲートが適用できます。
+
+
+```python
+qc = QuantumCircuit(2)
+qc.h(0)
+qc.h(1)
+qc.cz(0,1)
+
+backend = Aer.get_backend('statevector_simulator')
+final_state = execute(qc,backend).result().get_statevector()
+array_to_latex(final_state, pretext="\\\\\\\\text{Statevector} = ")
+```
+
+
+$\displaystyle 
+\\\\text{Statevector} = \begin{bmatrix}
+\tfrac{1}{2} \\
+\tfrac{1}{2} \\
+\tfrac{1}{2} \\
+-\tfrac{1}{2}
+\end{bmatrix}
+$
+
+
+
+```python
+qc.draw('mpl')
+```
+
+
+
+
+    
+![svg](base_nb_files/base_nb_14_0.svg)
+    
+
+
+
+### U_s
+
+$U_s$は、アダマールゲート => CZゲート => アダマールゲートの順番で適用すれば実現できます。
+
+$$
+U_{s} = H^{\otimes n} U_{0} H^{\otimes n}
 $$
 
 
-
-
 ```python
+qc = QuantumCircuit(2)
+qc.h(0)
+qc.h(1)
+qc.z(0)
+qc.z(1)
+qc.cz(0,1)
+qc.h(0)
+qc.h(1)
 
-```
-
-
-```python
-
-```
-
-
-```python
-qpe = QuantumCircuit(4, 3)
-qpe.x(3)
-qpe.draw('mpl')
+qc.draw('mpl')
 ```
 
 
 
 
     
-![svg](base_nb_files/base_nb_8_0.svg)
+![svg](base_nb_files/base_nb_16_0.svg)
     
 
 
 
-
-```python
-for qubit in range(3):
-    qpe.h(qubit)
-qpe.draw('mpl')
-```
-
-
-
-
-    
-![svg](base_nb_files/base_nb_9_0.svg)
-    
-
-
-
-
-```python
-repetitions = 1
-for counting_qubit in range(3):
-    for i in range(repetitions):
-        qpe.cp(math.pi/4, counting_qubit, 3);
-    repetitions *= 2
-qpe.draw('mpl')
-```
-
-
-
-
-    
-![svg](base_nb_files/base_nb_10_0.svg)
-    
-
-
+### 測定
+$U_s$と$U_f$を２量子ビットに適用し、グローバーのアルゴリズムが成り立つことを確認してみます。
 
 
 ```python
 
+qc = QuantumCircuit(2)
+qc.h(0)
+qc.h(1)
+qc.cz(0,1)
+
+qc.h(0)
+qc.h(1)
+qc.z(0)
+qc.z(1)
+qc.cz(0,1)
+qc.h(0)
+qc.h(1)
+
+backend = Aer.get_backend('statevector_simulator')
+final_state = execute(qc,backend).result().get_statevector()
+array_to_latex(final_state, pretext="\\\\\\\\text{Statevector} = ")
 ```
 
 
-```python
-if 1 <= 2:
-  print('aa')
-  pass
-```
+$\displaystyle 
+\\\\text{Statevector} = \begin{bmatrix}
+0 \\
+0 \\
+0 \\
+1
+\end{bmatrix}
+$
 
-    aa
 
-
-## 感想
+となり、ターゲットであった$|11\rangle$の測定確率が最大となりました。
--- rec/qiskit/base9/base_nb.py
+++ rec/qiskit/base9/base_nb.py
@@ -10,7 +10,7 @@
 # 
 # - https://qiskit.org/textbook/ja/ch-applications/hhl_tutorial.html
 # 
-# 私の拙いブログでqiskitがRec（推薦システム）のカテゴライズしいたのは、すべてHHLを理解するためでした。現在、推薦システムに興味があり、開発などを行っていますが、そこで重要なのが連立一次方程式を解く事です。連立一次方程式は、数理モデルをコンピュータを利用してとく場合に高い確率で利用されますが、推薦システムもUser-Item行列から如何にしてユーザーエンゲージメントの高い特徴量を抽出出来るかという事が重要になってきます。
+# 私の拙いブログでqiskitがRec（推薦システム）のカテゴライズしいたのは、すべてHHLを理解するためでした。現在、推薦システムに興味があり、開発などを行っていますが、そこで重要なのが連立一次方程式を解く事です。連立一次方程式は、数理モデルをコンピュータを利用して解く場合に高い確率で利用されますが、推薦システムもUser-Item行列から如何にしてユーザーエンゲージメントの高い特徴量を抽出出来るかという事が重要になってきます。
 # 
 # よって、量子コンピュータを利用して高速に連立一次方程式を解く事を目標に量子アルゴリズムの復習を開始したわけですが、ようやく目的までたどり着きました。
 # 
@@ -58,28 +58,369 @@ from qiskit.visualization import plot_histogram
 dict(qiskit.__qiskit_version__)
 
 
+# ## 共役勾配法
+# 
+# 復習の意味を込めて、古典アルゴリズムである共役勾配法の復習をします。
+# 正定値行列である$A$を係数とする連立一次方程式、
+# 
+# $$
+# A \boldsymbol{x}=\boldsymbol{b}
+# $$
+# 
+# の解$x$を反復法を用いて数値計算的に解く方法になります。反復法ですので、計算の終了を判定する誤差$(\epsilon)$が必要になります。
+
+# $A$,$x$,$b$は以下の様な行列になります。
+# 
+# $$
+# A = \left(\begin{array}{cccc}
+# a_{11} & a_{12} & \cdots & a_{1 n} \\
+# a_{21} & a_{22} & \cdots & a_{2 n} \\
+# \vdots & \vdots & \ddots & \vdots \\
+# a_{n 1} & a_{n 2} & \cdots & a_{n n}
+# \end{array}\right),\quad x=\left(\begin{array}{c}
+# x_{1} \\
+# x_{2} \\
+# \vdots \\
+# x_{n}
+# \end{array}\right), \quad b=\left(\begin{array}{c}
+# b_{1} \\
+# b_{2} \\
+# \vdots \\
+# b_{n}
+# \end{array}\right)
+# $$
+
+# 行列の表式で書くと以下の通りです。
+# 
+# $$
+# \left(\begin{array}{cccc}
+# a_{11} & a_{12} & \cdots & a_{1 n} \\
+# a_{21} & a_{22} & \cdots & a_{2 n} \\
+# \vdots & \vdots & \ddots & \vdots \\
+# a_{n 1} & a_{n 2} & \cdots & a_{n n}
+# \end{array}\right)\left(\begin{array}{c}
+# x_{1} \\
+# x_{2} \\
+# \vdots \\
+# x_{n}
+# \end{array}\right)=\left(\begin{array}{c}
+# b_{1} \\
+# b_{2} \\
+# \vdots \\
+# b_{n}
+# \end{array}\right)
+# $$
+
+# 次に次のように定義される関数$f(x)$を考えます。
+# 
+# $$
+# f(\boldsymbol{x})=\frac{1}{2}(\boldsymbol{x}, A \boldsymbol{x})-(\boldsymbol{b}, \boldsymbol{x})
+# $$
+# 
+# $(-,-)$は、ベクトルの内積を計算する演算子です。
+# 
+# $$
+# (\boldsymbol{x}, \boldsymbol{y})=\boldsymbol{x}^{T} \boldsymbol{y}=\sum_{i=1}^{n} \boldsymbol{x}_{i} \boldsymbol{y}_{i}
+# $$
+# 
+# 成分で表示すると以下の様になります。
+# 
+# $$
+# f(x)=\frac{1}{2} \sum_{i=1}^{n} \sum_{j=1}^{n} a_{i j} x_{i} x_{j}-\sum_{i=1}^{n} b_{i} x_{i}
+# $$
+
+# ここで、　$x_k$で微分すると、
+# 
+# $$
+# \frac{\partial f(x)}{\partial x_{k}}=\frac{1}{2} \sum_{i=1}^{n} a_{i k} x_{i}+\frac{1}{2} \sum_{j=1}^{n} a_{k j} x_{j}-b_{k}
+# $$
+# 
+# となります。
+
+# $A$はエルミート行列なので、
+# 
+# $$
+# \frac{\partial f(x)}{\partial x_{i}}=\sum_{j=1}^{n} a_{i j} x_{j}-b_{i}=0
+# $$
+# 
+# となります。
+
+# これを一般化すると、
+# 
+# $$
+# \nabla f(x)=\left(\begin{array}{c}
+# \frac{\partial f}{\partial x_{1}} \\
+# \vdots \\
+# \frac{\partial f}{\partial x_{n}}
+# \end{array}\right)=A\boldsymbol{x}-b = 0
+# $$
+# 
+# となり、関数$f(x)$の最小値となる$x$を求める事が、$A\boldsymbol{x}-b = 0$を解く事と同じである事が分かります。
+
+# ### アルゴリズム
+# 
+# 上記の通り、共役勾配法(CG法)は、関数$f(x)$を最小化することに帰着されます。
+# そのために、ある$x^{(0)}$を出発点に、以下の漸化式に従って最小値とする$x$を求めます。
+# 
+# $$
+# x^{(k+1)}=x^{(k)}+\alpha_{k} p^{(k)}
+# $$
+# 
+# ここで、$p^{k}$は解を探索する方向ベクトルです。
+# 
+# 
+# $$
+# f\left(x^{(k)}+\alpha_{k} p^{(k)}\right)=\frac{1}{2} \alpha_{k}{ }^{2}\left(p^{(k)}, A p^{(k)}\right)-\alpha_{k}\left(p^{(k)}, b-A x^{(k)}\right)+f\left(x^{(k)}\right)
+# $$
+
+# $$
+# \frac{\partial f\left(x^{(k)}+\alpha_{k} p^{(k)}\right)}{\partial \alpha_{k}}=0 \Rightarrow \alpha_{k}=\frac{\left(p^{(k)}, b-A x^{(k)}\right)}{\left(p^{(k)}, A p^{(k)}\right)}=\frac{\left(p^{(k)}, r^{(k)}\right)}{\left(p^{(k)}, A p^{(k)}\right)}
+# $$
+
+# $$
+# \begin{aligned}
+# &r^{(k+1)}=b-A x^{(k+1)} \\
+# &r^{(k)}=b-A x^{(k)} \\
+# &r^{(k+1)}-r^{(k)}=A x^{(k+1)}-A x^{(k)}=\alpha_{k} A p^{(k)}
+# \end{aligned}
+# $$
+
+# $$
+# \alpha_{k}=\frac{\left(p^{(k)}, r^{(k)}\right)}{\left(p^{(k)}, A p^{(k)}\right)}
+# $$
+
+# $$
+# \left\|\boldsymbol{r}^{(k+1)}\right\|<\varepsilon
+# $$
+
+# $$
+# \boldsymbol{p}^{(k+1)}=\boldsymbol{r}^{(k+1)}+\beta^{(k)} \boldsymbol{p}^{(k)}
+# $$
+
 # In[ ]:
 
 
 
 
 
-# ## 理論部分の理解
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# $$
+# p^{k}=-\nabla f\left(x^{k}\right)=\left(\frac{\partial f\left(x^{k}\right)}{\partial x^{1}}, \ldots, \frac{\partial f\left(x^{k}\right)}{\partial x^{n}}\right)^{T}
+# $$
+
 # 
-# HHLの仮定
+
+# ### 計算量
+# $s$は行列$A$の非0要素の割合、$\kappa$は行列$A$の最大固有値と最小固有値の比$\displaystyle \left| \frac{\lambda_{max}}{\lambda_{min}}\right|$、$\epsilon$  は精度です．
+# 
+# $$
+# O(N s \kappa \log (1 / \varepsilon))
+# $$
+# 
+# これは$N$に比例する形になっているため、$s\sim \log N$の場合、$N\log N$に比例する事になります。
+
+# ## HHLアルゴリズム
+# 
+# ### HHLの仮定
+# 
+# HHLはある程度の仮定の下に成立するアルゴリズムです。
+# 
 # - ローディングを実施する効果的なオラクルが存在
 # - ハミルトニアンシミュレーションと解の関数の計算が可能
 # - $A$がエルミート行列
 # - $\mathcal{O}\left(\log (N) s^{2} \kappa^{2} / \epsilon\right)$で計算可能
 # - 古典アルゴリズムが完全解を返すが、HHLは解となるベクトルを与える関数を近似するだけ
 
+# ### 量子回路へのマッピング
+# 
+# 連立一次方程式を量子アルゴリズムで解くには、$Ax=b$を量子回路にマッピングする必要があります。それは、$b$の$i$番目の成分は量子状態$|b\rangle$の$i$番目の基底状態の振幅に対応させるという方法です。また、当然ですが、その際は$\displaystyle \sum_i |b_i|^2 = 1$という規格化が必要です。
+# 
+# $$
+# Ax=b \rightarrow A|x\rangle = |b\rangle
+# $$
+# 
+# となります。
+
+# ### スペクトル分解
+# 
+# $A$はエルミート行列なので、スペクトル分解が可能です。$A$のエルミート性を暗幕的に仮定していましたが、
+# $$
+# A'=\left(\begin{array}{ll}
+# 0 & A \\
+# A & 0
+# \end{array}\right)
+# $$
+# 
+# とすれば、$A'$はエルミート行列となるため、問題ありません。よって、$A$は固有ベクトル$|u_i\rangle$とその固有値$\lambda_i$を利用して、以下の様に展開できます。
+# 
+# $$
+# A=\sum_{j=0}^{N-1} \lambda_{j}\left|u_{j}\right\rangle\left\langle u_{j}\right|
+# $$
+# 
+# よって、逆行列は以下の様になります。
+# 
+# $$
+# A^{-1}=\sum_{j=0}^{N-1} \lambda_{j}^{-1}\left|u_{j}\right\rangle\left\langle u_{j}\right|
+# $$
+
+# $u_i$は$A$の固有ベクトルなので、$|b\rangle$はその重ね合わせで表現できます。おそらくこれが量子コンピュータを利用する強い動機になっていると思います。
+# 
+# $$
+# |b\rangle=\sum_{j=0}^{N-1} b_{j}\left|u_{j}\right\rangle
+# $$
+# 
+# 本来であれば、$A$の固有ベクトルを計算できなければこの形で$|b\rangle$を用意することが出来ませんが、量子コンピュータでは$|b\rangle$を読み込むことで、自動的にこの状態を用意することが出来ます。
+# 
+# 最終的には以下の式の形を量子コンピュータを利用して求める事になります。
+# 
+# $$
+# |x\rangle=A^{-1}|b\rangle=\sum_{j=0}^{N-1} \lambda_{j}^{-1} b_{j}\left|u_{j}\right\rangle
+# $$
+
+# ### 1. データのロード
+# 
+# 対象となる$|b\rangle$の各データの振幅を量子ビット$n_b$にロードします。
+# 
+# $$
+# |0\rangle_{n_{b}} \mapsto|b\rangle_{n_{b}}
+# $$
+
+# ### 2. QPEの適用
+# 
+# 量子位相推定を利用して、ユニタリ演算子$U=e^{i A t}$ の$|b\rangle$の位相を推定します。
+# 
+# $|b\rangle$は上述の様に以下になります。
+# 
+# $$
+# |b\rangle=\sum_{j=0}^{N-1} b_{j}\left|u_{j}\right\rangle
+# $$
+
+# $U$は展開して整理すると、以下の様になります。
+# 
+# $$
+# \begin{aligned}
+# &U=e^{i A t}=\sum_{k=0}^{\infty} \frac{\left(i A t\right)^{k}}{k !}\\
+# &=\sum_{k=0}^{\infty} \frac{\left(i t\right)^{k}}{k !}\left(\sum_{j=0}^{N-1} \lambda_{j}|u_{j}\rangle\langle u_{j} |\right)^{k}\\
+# &=\sum_{k=0}^{\infty} \frac{(i t)^{k}}{k !} \sum_{j=0}^{N-1} \lambda_{j}^{k}\left|u_{j}\right\rangle \langle u_{j} |\\
+# &=\sum_{j=0}^{N-1}\left(\sum_{k=0}^{\infty} \frac{\left(i t\right)^{k}}{k_{i}} \lambda_{j}^{k}\right)|u_{j}\rangle \langle u_{j}| \\
+# &=\sum_{r=0}^{N-1} e^{i \lambda_{j} t}\left|u_{j}\right\rangle\left\langle u_{j}\right|
+# \end{aligned}
+# $$
+
+# $U$を$|b\rangle$に作用させると、
+# 
+# $$
+# \begin{aligned}
+# U|b\rangle &=U\left(\sum_{j=0}^{N-1} b_{j}\left|u_{j}\right\rangle\right) \\
+# &=\sum_{j^{\prime}=0}^{N-1} e^{i \lambda j^{\prime} t}\left|h_{j^{\prime}}\right\rangle\left\langle h_{j^{\prime}}\right| \cdot\left(\sum_{j=0}^{N-1} b_{j}\left|h_{j}\right\rangle\right) \\
+# &=\sum_{j=0}^{N-1} b_{j} e^{i \lambda_{j} t}\left|u_{j}\right\rangle
+# \end{aligned}
+# $$
+# 
+# となり、量子位相推定を利用して、$\lambda_j$の量子状態$|\tilde{\lambda}_j\rangle_{n_l}$を求める事が出来ます。
+# 
+# $\tilde{\lambda_{j}}$は、$\displaystyle 2^{n_{l}} \frac{\lambda_{j} t}{2 \pi}$に対する$n_l$-bitバイナリ近似となります。
+
+# $t=2\pi$とし、$\lambda_l$が、$n_l$ビットで正確に表現できるとすると、量子位相推定は以下の様に表現できます。
+# 
+# $$
+# \operatorname{QPE}\left(e^{i A 2 \pi}, \sum_{j=0}^{N-1} b_{j}|0\rangle_{n_{l}}\left|u_{j}\right\rangle_{n_{b}}\right)=\sum_{j=0}^{N-1} b_{j}\left|\lambda_{j}\right\rangle_{n_{l}}\left|u_{j}\right\rangle_{n_{b}}
+# $$
+
 # In[ ]:
 
 
 
 
 
-# $\tilde{\theta}$ は $2^{n} \theta$ に対するバイナリー近似を示しており、 $n$ の添え文字は $n$ デ ィジットに切り捨てられていることを示しています。
+# $b(\cdots)$は二進数表記である事を示し、$b_k(\cdots)$は、二進数表記の$k$ビット目の値を表します。まとめると、以下の様になります。
+# 
+# $$
+# \begin{aligned}
+# &b\left(\frac{1}{\pi} \cos ^{-1}\left(v_{i}\right)\right)=d_{0} d_{1} d_2 \cdot \cdots d_{m-1} \\
+# &b_{k}\left(\frac{1}{\pi} \cos ^{-1}\left(v_{i}\right)\right)=d_k \\
+# &\frac{2}{\pi} \cos ^{-1}\left(v_{i}\right)=\frac{d_{0}}{2}+\frac{d_{1}}{4}+\frac{d_{2}}{8} \cdots \frac{d_{m-1}}{2^{m}} \quad \left(0 \leqq \frac{2}{\pi} \cos ^{-1}\left(v_{i}\right) \leqq 1\right)
+# \end{aligned}
+# $$
+# 
+# これから、
+# 
+# 
+# $$
+# \begin{aligned}
+# \frac{2}{\pi} \cos ^{-1}\left(v_{i}\right)&=\frac{1}{2} b_{0}\left(\frac{1}{\pi} \cos ^{-1}\left(v_{1}\right)\right)+\frac{1}{4} b_{1}\left(\frac{1}{\pi} \cos ^{-1}\left(v_{i}\right)\right)+ \cdots \\
+# &=\sum_{k=0}^{m-1} b_{k}\left(\frac{1}{\pi} \cos ^{-1}\left(v_{n}\right)\right) 2^{-k-1} \cdots (1)
+# \end{aligned}
+# $$
+
+# $\displaystyle b_{k}\left(\frac{2}{\pi} \cos ^{-1}\left(v_{i}\right)\right)$をターゲットビットとして、$R_{y}\left(2^{-k-1}\pi\right)$である制御回転ゲートをかけることを考えます。
+# 
+# $$
+# \begin{aligned}
+# &\prod_{k=0}^{m-1} R_{y}\left(b_{k}\left(\frac{1}{\pi} \cos ^{-1}\left(v_{i}\right)\right) 2^{-k-1}\pi\right)|0\rangle \\
+# &=R_{y}\left(\sum_{k=0}^{m-1} b_{k}\left(\frac{1}{\pi} \cos ^{-1}\left(v_{i}\right)\right) 2^{-k-1}\pi\right)|0\rangle \\
+# \end{aligned}
+# $$
+
+# 回転ゲートは、以下の様にになります。
+# 
+# $$
+# R_{y}(\theta)|0\rangle=\cos \frac{\theta}{2}|0\rangle+\sin \frac{\theta}{2}|1\rangle
+# $$
+
+# この式と(1)を利用して、
+# 
+# $$
+# \begin{aligned}
+# &\cos\frac{1}{2}\left(\sum_{k=0}^{m-1} b_{k}\left(\frac{1}{\pi} \cos ^{-1}\left(v_{i}\right)\right) 2^{-k-1}\pi\right) \\
+# &=\cos\left(\frac{1}{2}\times\frac{2}{\pi} \cos ^{-1}\left(v_{i}\right)\times \pi\right) = v_i
+# \end{aligned}
+# $$
+
+# となり、こちらを利用して、
+# 
+# $$
+# R_{y}\left(\sum_{k=0}^{m-1} b_{k}\left(\frac{1}{\pi} \cos ^{-1}\left(v_{i}\right)\right) 2^{-k-1}\pi\right)|0\rangle=v_{i}|0\rangle+\sqrt{1-v_{i}^{2}}|1\rangle
+# $$
+# 
+# を得る。ここで、$\displaystyle v_i = \frac{1}{\lambda_i}$とすることで、補助ビットを、
+# 
+# $$
+# \sqrt{1-\frac{C^{2}}{\lambda_{j}^{2}}}|0\rangle+\frac{C}{\lambda_{j}}|1\rangle
+# $$
+# 
+# と計算することが出来る。
+
+# 
+
+# In[ ]:
+
+
+
+
 
 # $$
 # \operatorname{QPE}\left(U,|0\rangle_{n}|\psi\rangle_{m}\right)=|\tilde{\theta}\rangle_{n}|\psi\rangle_{m}
@@ -91,8 +432,268 @@ dict(qiskit.__qiskit_version__)
 
 
 
+# ## 計算量の比較
+# 
+# ## 量子
+# 
+# 
+# $$
+# O(s \kappa \operatorname{poly} \log (s \kappa / \varepsilon)))
+# $$
+# 
+# 行列 $A$ がスパース $(s \sim O(\operatorname{poly} \log N))$なら、
+# 
+# $$
+# O(s \kappa \operatorname{poly} \log (s \kappa / \varepsilon))) \sim O(s \kappa \operatorname{poly} \log N \operatorname{poly} \log (s \kappa / \varepsilon))
+# $$
+# 
+# 
+# ### 古典
+# $$
+# O(N s \kappa \log (1 / \varepsilon))
+# $$
+
+# ## qiskitで実装
+# 
+
+# In[84]:
+
+
+from qiskit import Aer
+from qiskit.circuit.library import QFT
+from qiskit.aqua import QuantumInstance, aqua_globals
+from qiskit.quantum_info import state_fidelity
+from qiskit.aqua.algorithms import HHL, NumPyLSsolver
+from qiskit.aqua.components.eigs import EigsQPE
+from qiskit.aqua.components.reciprocals import LookupRotation
+from qiskit.aqua.operators import MatrixOperator
+from qiskit.aqua.components.initial_states import Custom
+import numpy as np
+
+
+# In[85]:
+
+
+def create_eigs(matrix, num_ancillae, num_time_slices, negative_evals):
+    ne_qfts = [None, None]
+    if negative_evals:
+        num_ancillae += 1
+        ne_qfts = [QFT(num_ancillae - 1), QFT(num_ancillae - 1).inverse()]
+
+    return EigsQPE(MatrixOperator(matrix=matrix),
+                   QFT(num_ancillae).inverse(),
+                   num_time_slices=num_time_slices,
+                   num_ancillae=num_ancillae,
+                   expansion_mode='suzuki',
+                   expansion_order=2,
+                   evo_time=None, # np.pi*3/4, #None,  # This is t, can set to: np.pi*3/4
+                   negative_evals=negative_evals,
+                   ne_qfts=ne_qfts)
+
+
+# In[86]:
+
+
+def fidelity(hhl, ref):
+    solution_hhl_normed = hhl / np.linalg.norm(hhl)
+    solution_ref_normed = ref / np.linalg.norm(ref)
+    fidelity = state_fidelity(solution_hhl_normed, solution_ref_normed)
+    print("Fidelity:\t\t %f" % fidelity)
+
+
+# In[87]:
+
+
+matrix = [[1, -1/3], [-1/3, 1]]
+vector = [1, 0]
+
+
+# In[95]:
+
+
+orig_size = len(vector)
+matrix, vector, truncate_powerdim, truncate_hermitian = HHL.matrix_resize(matrix, vector)
+
+# Initialize eigenvalue finding module
+eigs = create_eigs(matrix, 3, 100, False)
+num_q, num_a = eigs.get_register_sizes()
+
+# Initialize initial state module
+init_state = Custom(num_q, state_vector=vector)
+# init_state = QuantumCircuit(num_q).initialize(vector/np.linalg.norm(vector))
+
+# Initialize reciprocal rotation module
+reci = LookupRotation(negative_evals=eigs._negative_evals, evo_time=eigs._evo_time)
+
+algo = HHL(matrix, vector, truncate_powerdim, truncate_hermitian, eigs,
+           init_state, reci, num_q, num_a, orig_size)
+
+
+# In[96]:
+
+
+result = algo.run(QuantumInstance(Aer.get_backend('statevector_simulator')))
+print("Solution:\t\t", np.round(result['solution'], 5))
+
+result_ref = NumPyLSsolver(matrix, vector).run()
+print("Classical Solution:\t", np.round(result_ref['solution'], 5))
+
+print("Probability:\t\t %f" % result['probability_result'])
+fidelity(result['solution'], result_ref['solution'])
+
+
+# In[92]:
+
+
+print("circuit_width:\t", result['circuit_info']['width'])
+print("circuit_depth:\t", result['circuit_info']['depth'])
+print("CNOT gates:\t", result['circuit_info']['operations']['cx'])
+
+
 # In[ ]:
 
 
 
 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[32]:
+
+
+from qiskit import aqua
+aqua.__version__
+
+
+# In[ ]:
+
+
+
+
+
+# In[65]:
+
+
+get_ipython().system('pip install --upgrade qiskit')
+
+
+# In[13]:
+
+
+get_ipython().system('pip install qiskit-terra')
+
+
+# In[ ]:
+
+
+
+
+
+# In[42]:
+
+
+get_ipython().run_line_magic('pinfo', 'Custom')
+
+init signature:
+Custom(
+    num_qubits: int,
+    state: str = 'zero',
+    state_vector: Union[numpy.ndarray, qiskit.aqua.operators.state_fns.state_fn.StateFn, NoneType] = None,
+    circuit: Union[qiskit.circuit.quantumcircuit.QuantumCircuit, NoneType] = None,
+) -> None
+
+
+# In[48]:
+
+
+get_ipython().run_line_magic('pinfo', 'QuantumCircuit.initialize')
+
+Args:
+    params (str or list or int):
+        * str: labels of basis states of the Pauli eigenstates Z, X, Y. See
+            :meth:`~qiskit.quantum_info.states.statevector.Statevector.from_label`.
+            Notice the order of the labels is reversed with respect to the qubit index to
+            be applied to. Example label '01' initializes the qubit zero to `|1>` and the
+            qubit one to `|0>`.
+        * list: vector of complex amplitudes to initialize to.
+        * int: an integer that is used as a bitmap indicating which qubits to initialize
+           to `|1>`. Example: setting params to 5 would initialize qubit 0 and qubit 2
+           to `|1>` and qubit 1 to `|0>`.
+    qubits (QuantumRegister or int):
+        * QuantumRegister: A list of qubits to be initialized [Default: None].
+        * int: Index of qubit to initialized [Default: None].
+
+Returns:
+    qiskit.circuit.Instruction: a handle to the instruction that was just initialized
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[103]:
+
+
+import math
+
+qc = QuantumCircuit(2)
+qc.h(0)
+qc.x(1)
+# Add Controlled-T
+qc.cp(math.pi/4, 0, 1)
+display(qc.draw())
+
+
+# In[104]:
+
+
+from qiskit import QuantumCircuit, Aer, assemble
+from math import pi
+import numpy as np
+from qiskit.visualization import plot_bloch_multivector, plot_histogram, array_to_latex
+
+qc.save_unitary()
+usim = Aer.get_backend('aer_simulator')
+qobj = assemble(qc)
+unitary = usim.run(qobj).result().get_unitary()
+array_to_latex(unitary, prefix="\\text{Circuit = }\n")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# ## 参考文献
+# - https://www2.yukawa.kyoto-u.ac.  jp/~qischool2019/mitaraiCTO.pdf
