3bb5c138a8e2e7355e1a16d11011c1b2cbf940f4
hiroshi.wayama <hiroshi.wayama@gmail.com>
2022-02-08 00:52:19 +0900

    update

---
+++ article/library/python/017/017_nb.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Python Tips
+# 
+# pythonを利用する上で、便利な表記などの個人的なメモです。基本的な部分は触れていません。対象も自分が便利だなと思ったものに限定されます。
+# 
+# ### github
+# - githubのjupyter notebook形式のファイルは[こちら](https://github.com/hiroshi0530/wa-src/blob/master/article/library/python/017/017_nb.ipynb)
+# 
+# ### google colaboratory
+# - google colaboratory で実行する場合は[こちら](https://colab.research.google.com/github/hiroshi0530/wa-src/blob/master/article/library/python/017/017_nb.ipynb)
+# 
+# ### 筆者の環境
+
+# In[1]:
+
+
+get_ipython().system('sw_vers')
+
+
+# In[2]:
+
+
+get_ipython().system('python -V')
+
+
+# In[ ]:
+
+
+
+
+
+# ## 参考記事
+
+# In[ ]:
+
+
+
+
